bc0e29865b38dbb01a02451dd246d375b57c6b6d
JQQQ <jiqiang90@hotmail.com>
2021-01-07 13:44:41 +1300

    [node] support load subquery project from tarball

--- packages/node/src/configure/project.model.ts
+++ packages/node/src/configure/project.model.ts
@@ -1,13 +1,41 @@
 // Copyright 2020-2021 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { ProjectManifest, SubqlDataSource } from '@subql/common';
+import fs from 'fs';
+import path from 'path';
+import os from 'os';
+import tar from 'tar';
+import {
+  loadProjectManifest,
+  ProjectManifest,
+  SubqlDataSource,
+} from '@subql/common';
 
 export class SubqueryProject implements ProjectManifest {
-  static create(source: ProjectManifest, path: string): SubqueryProject {
+  static async processPath(projectPath: string): Promise<string> {
+    const stats = fs.statSync(projectPath);
+    if (stats.isFile()) {
+      try {
+        const sep = path.basename(projectPath);
+        const tmpDir = os.tmpdir();
+        const tempPath = fs.mkdtempSync(`${tmpDir}${sep}`);
+        await tar.x({ file: projectPath, cwd: tempPath });
+        return tempPath.concat('/package');
+      } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+      }
+    } else if (stats.isDirectory()) {
+      return projectPath;
+    }
+  }
+
+  static async create(path: string): Promise<SubqueryProject> {
+    const projectPath = await this.processPath(path);
     const project = new SubqueryProject();
+    const source = loadProjectManifest(projectPath);
     Object.assign(project, source);
-    project.path = path;
+    project.path = projectPath;
     return project;
   }
 
