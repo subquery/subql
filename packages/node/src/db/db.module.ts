41212ffd1786bd8dda97103cbc7e7b44fe4a0b22
JQQQ <jiqiang90@hotmail.com>
2021-01-11 16:25:36 +1300

    [node] Support node retry to establish connection with postgres (#61)
    * [node] Support node retry to establish connection with postgres
    
    * [node] move retries to catch block

--- packages/node/src/db/db.module.ts
+++ packages/node/src/db/db.module.ts
@@ -1,6 +1,5 @@
 // Copyright 2020-2021 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
-
 import { DynamicModule, Global } from '@nestjs/common';
 import { Sequelize } from 'sequelize';
 import * as entities from '../entities';
@@ -13,6 +12,27 @@ export interface DbOption {
   database: string;
 }
 
+async function timeout(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function establishConnection(
+  sequelize: Sequelize,
+  numRetries: number,
+): Promise<void> {
+  try {
+    await sequelize.authenticate();
+  } catch (error) {
+    console.error('Unable to connect to the database', error.message);
+    if (numRetries > 0) {
+      await timeout(3000);
+      void (await establishConnection(sequelize, numRetries - 1));
+    } else {
+      process.exit(1);
+    }
+  }
+}
+
 const sequelizeFactory = (option: DbOption) => async () => {
   const sequelize = new Sequelize({
     dialect: 'postgres',
@@ -22,7 +42,8 @@ const sequelizeFactory = (option: DbOption) => async () => {
     host: option.host,
     port: option.port,
   });
-  await sequelize.authenticate();
+  const numRetries = 5;
+  await establishConnection(sequelize, numRetries);
   for (const factoryFn of Object.keys(entities).filter((k) =>
     /Factory$/.exec(k),
   )) {
