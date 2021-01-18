2f11785025731d48fd8144b1706adf5d7fdfd7d3
JQQQ <jiqiang90@hotmail.com>
2021-01-18 17:34:30 +1300

    [Docs] update docs to match with latest subql 0.5.x

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,11 +7,6 @@ module.exports = {
             title: 'SubQuery',
             description: 'SubQuery is a open-source tool to provide a complete process and query data solution to every substrate project and will become core infrastructure for the Polkadot ecosystem.'
         },
-        '/zh/': {
-            lang: '中文',
-            title: 'SubQuery',
-            description: 'SubQuery是一个开源工具，可以为每一个substrate项目提供完整的处理和查询解决方案，并将成为Polkadot生态系统的核心基础架构。',
-        }
     },
 
     themeConfig: {
@@ -31,36 +26,12 @@ module.exports = {
                         'quickstart',
                         'directory_structure',
                         'define_a_subquery' ,
-                        'deploy',
-                        'query_the_project',
-                        'qna'
-                    ]
+                    ],
                 }
 
             },
-            '/zh/': {
-
-                selectText: '选择语言',
-                label: '简体中文',
-                algolia: {},
-                nav: [
-                    { text: '项目指南', link: '/zh/' },
-                    { text: 'OnFinality', link: 'https://onfinality.io/', target:'_self', rel:''},
-
-                ],
-                sidebar: {
-                    '/zh/': [
-                        '',
-                        'quickstart',
-                        'directory_structure',
-                        'define_a_subquery' ,
-                        'deploy',
-                        'query_the_project',
-                        'qna'
-                    ]
-                }
-            }
-        }
+        },
+        sidebarDepth: 2
     }
 
 }
--- docs/directory_structure.md
+++ docs/directory_structure.md
@@ -1,17 +1,21 @@
 # Directory Structure
 
-We assume you have successful initialized the starter package in the [Quick start](/quickstart) section.
+We assume you have successfully initialized the starter package in the [Quick start](/quickstart) section.
 
+Following map provide an overview of a SubQuery project.
 
 ```
-- subql-starter
+- project-name
   L package.json
   L project.yaml
   L README.md
   L schema.graphql
   L tsconfig.json
+  L docker-compose.yml
   L src
     L index.ts
     L mappings
-      L Extrinsic.ts
+      L mappingHandlers.ts
 ```
+
+In the [Define a SubQuery](/define_a_subquery) section, we will explore more details of each file. 
