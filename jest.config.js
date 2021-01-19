87b9c2677a2a7a47878aefb798b3169333f6e102
Ian He <ian@onfinality.io>
2021-01-19 17:07:07 +1300

    set moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -89,6 +89,10 @@ module.exports = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
+  moduleNameMapper: {
+    "^@subql/common": '<rootDir>/packages/common/src',
+    "^@subql/common/(.*)$": '<rootDir>/packages/common/src/$1',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
