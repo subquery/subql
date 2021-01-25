4f39268cfeceddf44246e26c7ce4f3046edf4305
JQQQ <jiqiang90@hotmail.com>
2021-01-26 12:11:35 +1300

    [CLI] Tidy up init controller

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -5,7 +5,8 @@ import fs from 'fs';
 import path from 'path';
 import {Command, flags} from '@oclif/command';
 import cli from 'cli-ux';
-import {createProject, projectSpec} from '../controller/init-controller';
+import {createProject} from '../controller/init-controller';
+import {ProjectSpec} from '../types';
 
 export default class Init extends Command {
   static description = 'Init a scaffold subquery project';
@@ -26,23 +27,23 @@ export default class Init extends Command {
 
   async run(): Promise<void> {
     const {args, flags} = this.parse(Init);
-    const project = {} as projectSpec;
+    const project = {} as ProjectSpec;
 
     project.name = args.projectName
       ? args.projectName
-      : ((await cli.prompt('What is the project name?', {default: 'subql-starter', required: true})) as string);
+      : await cli.prompt('Project name', {default: 'subql-starter', required: true});
     if (fs.existsSync(path.join(process.cwd(), `${project.name}`))) {
       throw new Error(`Directory ${project.name} exists, try another project name`);
     }
-    project.repository = (await cli.prompt('What is the project git repository?', {required: false})) as string;
-    project.endpoint = (await cli.prompt('What is the RPC endpoint?', {
+    project.repository = await cli.prompt('Git repository', {required: false});
+    project.endpoint = await cli.prompt('RPC endpoint', {
       default: 'wss://polkadot.api.onfinality.io/public-ws',
       required: true,
-    })) as string;
-    project.author = (await cli.prompt('What is the authors name?', {required: true})) as string;
-    project.description = (await cli.prompt('Description', {required: false})) as string;
-    project.version = (await cli.prompt('Version:', {default: '1.0.0', required: true})) as string;
-    project.license = (await cli.prompt('License:', {default: 'Apache-2.0', required: true})) as string;
+    });
+    project.author = await cli.prompt('Authors', {required: true});
+    project.description = await cli.prompt('Description', {required: false});
+    project.version = await cli.prompt('Version:', {default: '1.0.0', required: true});
+    project.license = await cli.prompt('License:', {default: 'Apache-2.0', required: true});
 
     if (flags.starter && project.name) {
       cli.action.start('Init the starter package');
--- packages/cli/src/controller/init-controller.ts
+++ packages/cli/src/controller/init-controller.ts
@@ -7,21 +7,12 @@ import {promisify} from 'util';
 import yaml from 'js-yaml';
 import rimraf from 'rimraf';
 import simpleGit from 'simple-git';
+import {ProjectSpec} from '../types';
 
 const STARTER_PATH = 'https://github.com/OnFinality-io/subql-starter';
 
-export interface projectSpec {
-  name: string;
-  repository?: string;
-  endpoint: string;
-  author: string;
-  description?: string;
-  version: string;
-  license: string;
-}
-
-export async function createProject(localPath: string, project: projectSpec): Promise<void> {
-  const projectPath = path.join(localPath, `${project.name}`);
+export async function createProject(localPath: string, project: ProjectSpec): Promise<void> {
+  const projectPath = path.join(localPath, project.name);
   try {
     await simpleGit().clone(STARTER_PATH, projectPath);
   } catch (e) {
@@ -44,26 +35,27 @@ export async function createProject(localPath: string, project: projectSpec): Pr
   }
 }
 
-async function preparePackage(projectPath: string, project: projectSpec): Promise<void> {
+async function preparePackage(projectPath: string, project: ProjectSpec): Promise<void> {
   //load and write package.json
   const packageData = await fs.promises.readFile(`${projectPath}/package.json`);
   const currentPackage = JSON.parse(packageData.toString());
   currentPackage.name = project.name;
   currentPackage.version = project.version;
-  currentPackage.description = project.description ? project.description : currentPackage.description;
+  currentPackage.description = project.description ?? currentPackage.description;
   currentPackage.author = project.author;
   currentPackage.license = project.license;
   const newPackage = JSON.stringify(currentPackage, null, 2);
   await fs.promises.writeFile(`${projectPath}/package.json`, newPackage, 'utf8');
 }
 
-async function prepareManifest(projectPath: string, project: projectSpec): Promise<void> {
+async function prepareManifest(projectPath: string, project: ProjectSpec): Promise<void> {
   //load and write manifest(project.yaml)
-  const manifest = await fs.promises.readFile(`${projectPath}/project.yaml`, 'utf8');
+  const yamlPath = path.join(`${projectPath}`, `project.yaml`);
+  const manifest = await fs.promises.readFile(yamlPath, 'utf8');
   const data = yaml.safeLoadAll(manifest);
-  data[0].description = project.description ? project.description : data[0].description;
+  data[0].description = project.description ?? data[0].description;
   data[0].network.endpoint = project.endpoint;
-  data[0].repository = project.repository ? project.repository : '';
+  data[0].repository = project.repository ?? '';
   const newYaml = yaml.safeDump(data);
-  await fs.promises.writeFile(`${projectPath}/project.yml`, newYaml, 'utf8');
+  await fs.promises.writeFile(yamlPath, newYaml, 'utf8');
 }
