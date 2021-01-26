c4ddf42ce3598faa28d4854201c3e018b046fadd
JQQQ <jiqiang90@hotmail.com>
2021-01-26 16:01:10 +1300

    [CLI] Add user interaction for subql init (#118)
    * [CLI] Add user interaction for subql init
    
    * [CLI] Tidy up init controller

--- packages/cli/src/controller/init-controller.ts
+++ packages/cli/src/controller/init-controller.ts
@@ -2,24 +2,60 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import fs from 'fs';
+import * as path from 'path';
+import {promisify} from 'util';
+import yaml from 'js-yaml';
 import rimraf from 'rimraf';
-import simpleGit, {SimpleGit} from 'simple-git';
-const git: SimpleGit = simpleGit();
+import simpleGit from 'simple-git';
+import {ProjectSpec} from '../types';
 
-const starterPath = 'https://github.com/OnFinality-io/subql-starter';
+const STARTER_PATH = 'https://github.com/OnFinality-io/subql-starter';
 
-export async function createProject(projectName: string): Promise<void> {
-  const localPath = `${process.cwd()}/${projectName}`;
-  if (fs.existsSync(localPath)) {
-    throw new Error('Directory exists');
+export async function createProject(localPath: string, project: ProjectSpec): Promise<void> {
+  const projectPath = path.join(localPath, project.name);
+  try {
+    await simpleGit().clone(STARTER_PATH, projectPath);
+  } catch (e) {
+    throw new Error('Failed to clone starter template from git');
   }
-  await git.clone(starterPath, localPath);
-  const packageData = fs.readFileSync(`${localPath}/package.json`);
+  try {
+    await prepareManifest(projectPath, project);
+  } catch (e) {
+    throw new Error('Failed to prepare read or write manifest while preparing the project');
+  }
+  try {
+    await preparePackage(projectPath, project);
+  } catch (e) {
+    throw new Error('Failed to prepare read or write package.json while preparing the project');
+  }
+  try {
+    await promisify(rimraf)(`${projectPath}/.git`);
+  } catch (e) {
+    throw new Error('Failed to remove .git from starter project');
+  }
+}
+
+async function preparePackage(projectPath: string, project: ProjectSpec): Promise<void> {
+  //load and write package.json
+  const packageData = await fs.promises.readFile(`${projectPath}/package.json`);
   const currentPackage = JSON.parse(packageData.toString());
-  currentPackage.name = projectName;
+  currentPackage.name = project.name;
+  currentPackage.version = project.version;
+  currentPackage.description = project.description ?? currentPackage.description;
+  currentPackage.author = project.author;
+  currentPackage.license = project.license;
   const newPackage = JSON.stringify(currentPackage, null, 2);
-  fs.writeFileSync(`${localPath}/package.json`, newPackage, 'utf8');
-  await new Promise<void>((resolve, reject) =>
-    rimraf(`${localPath}/.git`, (error) => (error ? reject(error) : resolve()))
-  );
+  await fs.promises.writeFile(`${projectPath}/package.json`, newPackage, 'utf8');
+}
+
+async function prepareManifest(projectPath: string, project: ProjectSpec): Promise<void> {
+  //load and write manifest(project.yaml)
+  const yamlPath = path.join(`${projectPath}`, `project.yaml`);
+  const manifest = await fs.promises.readFile(yamlPath, 'utf8');
+  const data = yaml.safeLoadAll(manifest);
+  data[0].description = project.description ?? data[0].description;
+  data[0].network.endpoint = project.endpoint;
+  data[0].repository = project.repository ?? '';
+  const newYaml = yaml.safeDump(data);
+  await fs.promises.writeFile(yamlPath, newYaml, 'utf8');
 }
---
+++ packages/cli/src/types.ts
@@ -0,0 +1,12 @@
+// Copyright 2020-2021 OnFinality Limited authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+export interface ProjectSpec {
+  name: string;
+  repository?: string;
+  endpoint: string;
+  author: string;
+  description?: string;
+  version: string;
+  license: string;
+}
