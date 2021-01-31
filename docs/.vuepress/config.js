f505da0b6ed66f2bf96db4a9ae8a2e71e4ecd286
JQQQ <jiqiang90@hotmail.com>
2021-02-01 10:05:13 +1300

    [Doc] Edit title

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     locales: {
         '/': {
             lang: 'English',
-            title: 'SubQuery',
+            title: 'SubQuery Guide',
             description: 'SubQuery is a open-source tool to provide a complete process and query data solution to every substrate project and will become core infrastructure for the Polkadot ecosystem.'
         },
     },
@@ -19,7 +19,7 @@ module.exports = {
                 ariaLabel: 'Languages',
                 algolia: {},
                 nav: [
-                    { text: 'Home', link: 'https://www.subquery.network/',target:'_self', rel:''},
+                    { text: 'SubQuery', link: 'https://www.subquery.network/',target:'_self', rel:''},
                     { text: 'Guide', link: '/' },
                     { text: 'OnFinality', link: 'https://onfinality.io/', target:'_self', rel:''},
                 ],
