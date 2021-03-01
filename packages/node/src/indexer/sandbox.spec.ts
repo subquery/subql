ca1ff76c6f6fc8b2d58f32bc1222d9c8f9665d04
JQQQ <jiqiang90@hotmail.com>
2021-03-01 14:52:37 +1300

    [node] fix prettier

--- packages/node/src/indexer/sandbox.spec.ts
+++ packages/node/src/indexer/sandbox.spec.ts
@@ -14,12 +14,12 @@ describe('sandbox for subql-node', () => {
 
   it('resolve entry from package.json', () => {
     vm = new IndexerSandbox(
-        {
-          store: undefined,
-          api: undefined,
-          root: path.resolve(__dirname, '../../test/sandbox'),
-        },
-        new NodeConfig({ subquery: '', subqueryName: '' }),
+      {
+        store: undefined,
+        api: undefined,
+        root: path.resolve(__dirname, '../../test/sandbox'),
+      },
+      new NodeConfig({ subquery: '', subqueryName: '' }),
     );
     expect(vm.entry).toEqual('./main.js');
   });
