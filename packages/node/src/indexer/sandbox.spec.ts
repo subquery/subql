f44be1e3033d98b088adb86178f24e72e160bde8
JQQQ <jiqiang90@hotmail.com>
2021-03-01 13:41:14 +1300

    [node] fix for test

--- packages/node/src/indexer/sandbox.spec.ts
+++ packages/node/src/indexer/sandbox.spec.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import * as path from 'path';
+import {NodeConfig} from "../configure/NodeConfig";
 import { IndexerSandbox } from './sandbox';
 
 describe('sandbox for subql-node', () => {
@@ -21,11 +22,13 @@ describe('sandbox for subql-node', () => {
   });
 
   it('wait until promise resolved', async () => {
+
     vm = new IndexerSandbox({
       store: undefined,
       api: undefined,
       root: path.resolve(__dirname, '../../test/sandbox'),
-    });
+    }, new NodeConfig({ subquery: '', subqueryName: ''}),
+    );
     let sandboxFuncionEndAt: Date;
     vm.on('console.log', (line) => {
       if (line === 'OK') {
