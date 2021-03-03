ae70d0099e8a25476e412d0ee4e3b82a7dd02530
JQQQ <jiqiang90@hotmail.com>
2021-03-03 13:24:28 +1300

    [common] add get entities and relations for 1-1, and 1-N

--- packages/common/src/graphql/entities.ts
+++ packages/common/src/graphql/entities.ts
@@ -1,8 +1,24 @@
 // Copyright 2020-2021 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import {GraphQLSchema, GraphQLObjectType, isObjectType} from 'graphql';
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  isObjectType,
+  isNonNullType,
+  getNullableType,
+  isListType,
+  assertListType, getDirectiveValues, GraphQLField
+} from 'graphql';
 import {DirectiveName} from './constant';
+
+import {
+  GraphQLModelsRelations,
+  GraphQLModelsType,
+  GraphQLEntityField,
+  FieldScalar,
+  GraphQLRelationsType
+} from './types';
 
 export function getAllEntities(schema: GraphQLSchema): GraphQLObjectType[] {
   return Object.entries(schema.getTypeMap())
@@ -11,4 +27,100 @@ export function getAllEntities(schema: GraphQLSchema): GraphQLObjectType[] {
     .filter(isObjectType);
 }
 
-// TODO: GraphQLNamedType -> EntitySchema
+export function getAllEntitiesRelations(schema: GraphQLSchema): GraphQLModelsRelations{
+  const modelRelations = {models:[],relations:[]} as GraphQLModelsRelations;
+  const entityNameSet = [];
+  const reverseLookupRelations = [];
+
+  const derivedFrom = schema.getDirective('derivedFrom');
+  const entities = Object.entries(schema.getTypeMap())
+      .filter(([, node]) => node.astNode?.directives?.find(({name: {value}}) => value === DirectiveName.Entity))
+      .map(([, node]) => node)
+      .filter(isObjectType);
+
+  for (const entity of entities){
+    entityNameSet.push(entity.name);
+  }
+
+  for (const entity of entities){
+    const newModel: GraphQLModelsType=  {
+      name: entity.name,
+      fields : []
+    }
+    const fields = entity.getFields();
+    for (const k in fields){
+      if (Object.prototype.hasOwnProperty.call(fields, k)) {
+        const typeString = extractType(fields[k].type).toString();
+        //If is a basic scalar type
+        if (Object.values(FieldScalar).includes(typeString)) {
+          newModel.fields.push(packEntityField(typeString, fields[k],'basic'));
+        }
+        // If is a foreign key
+        else if (entityNameSet.includes(typeString) && fields[k].astNode.directives.length ===0){
+          newModel.fields.push(packEntityField(typeString, fields[k],'foreignKey'));
+          modelRelations.relations.push({
+            from:entity.name, //Identity
+            type: 'belongsTo',
+            to:typeString, //Account
+            foreignKey:  `${fields[k].name}Id` //accountId
+          } as GraphQLRelationsType)
+        }
+        // If is derivedFrom
+        else if (entityNameSet.includes(typeString) && fields[k].astNode.directives.length!==0){
+          const directValues = getDirectiveValues(derivedFrom, fields[k].astNode);
+          if (directValues.field){
+            reverseLookupRelations.push({
+              fromEntity:entity.name,  //Account
+              toEntity: typeString,     //Identity
+              type: isListType(isNonNullType(fields[k].type) ? getNullableType(fields[k].type) : fields[k].type)? 'hasMany':'hasOne',
+              foreignKey: `${directValues.field}Id`, //accountId
+              fieldName: fields[k].name, //identity
+            })
+          }
+        }
+      }
+    }
+    modelRelations.models.push(newModel);
+  }
+  handleDerived(reverseLookupRelations, modelRelations.relations);
+  return modelRelations;
+}
+
+
+function packEntityField(typeString:any, field:GraphQLField<any,any>, fieldType?:string) : GraphQLEntityField{
+  const entityField: GraphQLEntityField=  {
+    name: field.name,
+    type: typeString,
+    isArray: isListType(isNonNullType(field.type) ? getNullableType(field.type) : field.type),
+    nullable: !isNonNullType(field.type),
+  }
+  if(fieldType === "foreignKey"){
+    entityField.name = `${entityField.name}Id`; //Update name
+    entityField.type = 'String';
+  }
+  return entityField
+}
+//Get the type, ready to be convert to string
+function extractType(type:any){
+  const offNullType = isNonNullType(type) ?  getNullableType(type) : type ;
+  const offListType = isListType(offNullType)?assertListType(offNullType).ofType: type;
+  return isNonNullType(offListType)? getNullableType(offListType) : offListType;
+}
+
+function handleDerived(reverseLookupRelations,relations:GraphQLRelationsType[]):void{
+  for (const r of reverseLookupRelations){
+    for (const relation of relations){
+      if (r.fromEntity === relation.to
+          && r.toEntity === relation.from
+          && r.foreignKey === relation.foreignKey){
+        relations.push({
+          from:r.fromEntity,
+          type: r.type,
+          to: r.toEntity,
+          foreignKey:  r.foreignKey, //accountId
+          fieldName: r.fieldName,
+        } as GraphQLRelationsType)
+      }
+    }
+  }
+}
--- packages/common/src/graphql/graphql.spec.ts
+++ packages/common/src/graphql/graphql.spec.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import gql from 'graphql-tag';
-import {getAllEntities} from './entities';
+import {getAllEntities,getAllEntitiesRelations} from './entities';
 import {buildSchemaFromDocumentNode} from './schema';
 
 describe('utils that handle schema.graphql', () => {
@@ -32,7 +32,6 @@ describe('utils that handle schema.graphql', () => {
     expect(entities).toMatchObject([{name: 'KittyBirthInfo'}]);
     expect(Object.keys(entities[0].getFields())).toEqual(expect.arrayContaining(['id', 'birthBlockHeight', 'owner']));
   });
-
   it('throw error for unsupported types', () => {
     const graphqlSchema = gql`
       type Test @entity {
@@ -41,5 +40,60 @@ describe('utils that handle schema.graphql', () => {
       }
     `;
     expect(() => buildSchemaFromDocumentNode(graphqlSchema)).toThrow();
+    expect(() => buildSchemaFromDocumentNode(gql`
+      type Test @entity {
+        id: ID!
+        price: [String]
+      }
+    `)).toThrow();
   });
+
+  it('can extract list field with entity from the schema', () => {
+    const graphqlSchema = gql`
+      type KittyBirthInfo @entity {
+        id: ID!,
+        owner: String,
+        paper: paper @derivedFrom(field: "account")
+      }
+      type paper @entity {
+        id: ID!
+        account: KittyBirthInfo
+      }
+    `;
+    const schema = buildSchemaFromDocumentNode(graphqlSchema);
+    const entities = getAllEntitiesRelations(schema);
+    expect(entities.models).toMatchObject(
+        [{
+          name: 'KittyBirthInfo',
+          fields: [
+            { name: 'id', type: 'ID', isArray: false, nullable: false },
+            { name: 'owner', type: 'String', isArray: false, nullable: true }
+          ]
+        }, {name:'paper', fields: [
+            { name: 'id', type: 'ID', isArray: false, nullable: false },
+            {
+              name: 'accountId',
+              type: 'String',
+              isArray: false,
+              nullable: true
+            }
+          ]}]
+    );
+
+    expect(entities.relations).toMatchObject(
+        [{
+          from: 'paper',
+          type: 'belongsTo',
+          to: 'KittyBirthInfo',
+          foreignKey: 'accountId'
+        }, {
+          from: 'KittyBirthInfo',
+          type: 'hasOne',
+          to: 'paper',
+          foreignKey: 'accountId',
+          fieldName: 'paper'}]
+    );
+
+  });
+
 });
