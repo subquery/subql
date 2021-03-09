8dd5751c87b97992fe981585a62c7471ff40b490
JQQQ <jiqiang90@hotmail.com>
2021-03-10 11:24:49 +1300

    [node] change default startblock to 1 (#216)
    * change default startblock to 1
    * bool is supported

--- packages/node/src/configure/project.model.ts
+++ packages/node/src/configure/project.model.ts
@@ -7,7 +7,10 @@ import {
   SubqlDataSource,
 } from '@subql/common';
 import { ProjectNetwork } from '@subql/common/project/models';
+import { getLogger } from '../utils/logger';
 import { prepareProjectDir } from '../utils/project';
+
+const logger = getLogger('configure');
 
 export class SubqueryProject implements ProjectManifest {
   static async create(path: string): Promise<SubqueryProject> {
@@ -16,11 +19,16 @@ export class SubqueryProject implements ProjectManifest {
     const source = loadProjectManifest(projectPath);
     Object.assign(project, source);
     project.path = projectPath;
+    project.dataSources.map(function (dataSource) {
+      if (!dataSource.startBlock || dataSource.startBlock < 1) {
+        dataSource.startBlock = 1;
+        logger.warn('start block changed to #1');
+      }
+    });
     return project;
   }
 
   path: string;
-
   dataSources: SubqlDataSource[];
   description: string;
   network: ProjectNetwork;
