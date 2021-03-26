62d203b832b50efe615803393fd1213edd86bee2
JQQQ <jiqiang90@hotmail.com>
2021-03-27 09:41:15 +1300

    Format

--- packages/node/src/indexer/indexer.manager.ts
+++ packages/node/src/indexer/indexer.manager.ts
@@ -153,7 +153,9 @@ export class IndexerManager {
       )
       .map((item) => item.startBlock ?? 1);
     if (startBlocksList.length === 0) {
-      logger.error(`Failed to find a valid datasource, Please check your endpoint if specName filter is used.`);
+      logger.error(
+        `Failed to find a valid datasource, Please check your endpoint if specName filter is used.`,
+      );
       process.exit(1);
     } else {
       return Math.min(...startBlocksList);
