b7d7cfda768c5c1b79d04508874315eb0689fc00
JQQQ <jiqiang90@hotmail.com>
2021-06-02 10:29:13 +1200

    Adjust batch size

--- packages/node/src/configure/NodeConfig.ts
+++ packages/node/src/configure/NodeConfig.ts
@@ -31,7 +31,7 @@ export type MinConfig = Partial<Omit<IConfig, 'subqueryName' | 'subquery'>> &
 
 const DEFAULT_CONFIG = {
   localMode: false,
-  batchSize: 100,
+  batchSize: 30,
   timeout: 20,
   preferRange: false,
   debug: false,
