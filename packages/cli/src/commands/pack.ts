dc802a42f347de2081bd70c0d9d7cc5c8547a80e
Scott Twiname <skott.twiname@gmail.com>
2021-09-15 14:08:46 +1200

    Fix lint issue

--- packages/cli/src/commands/pack.ts
+++ packages/cli/src/commands/pack.ts
@@ -1,13 +1,13 @@
 // Copyright 2020-2021 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import {Command, flags} from '@oclif/command';
-import path from 'path';
 import child from 'child_process';
+import {lstatSync} from 'fs';
+import path from 'path';
+import {Command, flags} from '@oclif/command';
 import webpack from 'webpack';
 import {merge} from 'webpack-merge';
 import Validate from './validate';
-import {lstatSync} from 'fs';
 
 const getBaseConfig = (dir: string): webpack.Configuration => ({
   target: 'node',
