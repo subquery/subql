beff2eae88551886cf4e3404564fc425f7b9d3ac
JQQQ <jiqiang90@hotmail.com>
2021-09-24 16:26:43 +1200

    change `_meta` back to `_metadata`

---
+++ packages/query/src/graphql/plugins/smartTagsPlugin.ts
@@ -0,0 +1,14 @@
+// Copyright 2020-2021 OnFinality Limited authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import {PgEntity, PgEntityKind} from 'graphile-build-pg';
+import {makePgSmartTagsPlugin} from 'graphile-utils';
+
+export const smartTagsPlugin = makePgSmartTagsPlugin({
+  //Rule 1, omit `_metadata` from node
+  kind: PgEntityKind.CLASS,
+  match: ({name}: PgEntity) => /_metadata/.test(name),
+  tags: {
+    omit: true,
+  },
+});
