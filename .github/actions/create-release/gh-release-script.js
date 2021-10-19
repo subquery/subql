d608987fd6501f424a18dee7d3afa22192d933fc
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2021-10-19 21:27:44 +1300

    [SKIP CI] fix release script issue with changelog (#545)
    Co-authored-by: Lachlan McCrae <lachlanm@Lachlans-MacBook-Pro.local>

--- .github/actions/create-release/gh-release-script.js
+++ .github/actions/create-release/gh-release-script.js
@@ -10,6 +10,8 @@ const pJson = require(`${myArgs[0]}/package.json`)
 const version = pJson.version;
 const repoName = pJson.name; 
 
+const packageName = repoName.split('/');
+
 function checkForBetaVersion(version) {
     if (version.includes('-')){
         exit(0); //skip this package but continue trying to release others
@@ -21,6 +23,7 @@ function gatherReleaseInfo(logPath) {
     const regex = /## \[([0-9]+(\.[0-9]+)+)] - [0-9]{4}-[0-9]{2}-[0-9]{2}/i;
     
     let lines = changeLogs.split(/\n/);
+    let foundChangelog = false;
     let releaseInfo = '';
     let i = 0;
 
@@ -28,21 +31,24 @@ function gatherReleaseInfo(logPath) {
        if(lines[j].includes(`[${version}]`)){
            i = j;
            j = lines.length;
+           foundChangelog = true;
        } 
     }
 
     lines = lines.slice(i);
-    
-    for(let j = 0; j < lines.length; j++){
-        if(j == 0){
-           releaseInfo += `${lines[j]}`+ '\n';
-           continue;
-        }        
 
-        if(!regex.test(lines[j])){
-            releaseInfo += `${lines[j]}`+ '\n';
-        } else {
-            j = lines.length;
+    if(foundChangelog){
+        for(let j = 0; j < lines.length; j++){
+            if(j == 0){
+               releaseInfo += `${lines[j]}`+ '\n';
+               continue;
+            }        
+    
+            if(!regex.test(lines[j])){
+                releaseInfo += `${lines[j]}`+ '\n';
+            } else {
+                j = lines.length;
+            }
         }
     }
     
@@ -55,7 +61,6 @@ function gatherReleaseInfo(logPath) {
 }
 
 async function publishRelease(releaseInfo) {
-    const repoTagName = repoName.split('/');
 
     await request('POST /repos/{owner}/{repo}/releases', {
         headers: {
@@ -64,7 +69,7 @@ async function publishRelease(releaseInfo) {
         owner: 'subquery',
         name: `[${version}] ${repoName}`,
         repo: 'subql',
-        tag_name: `${repoTagName[1]}/${version}`,
+        tag_name: `${packageName[1]}/${version}`,
         body: releaseInfo
     }).catch( err => {
         core.setFailed(err)
@@ -75,6 +80,6 @@ async function publishRelease(releaseInfo) {
  
 checkForBetaVersion(version);
 
-const releaseInfo = gatherReleaseInfo(`./packages/common/CHANGELOG.md`);
+const releaseInfo = gatherReleaseInfo(`./packages/${packageName[1]}/CHANGELOG.md`);
 
 publishRelease(releaseInfo);
