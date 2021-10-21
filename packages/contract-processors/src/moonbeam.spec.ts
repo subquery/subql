7a05014e8d3f42b05292fd25fc3166a4cb386ae8
Scott Twiname <skott.twiname@gmail.com>
2021-10-21 14:14:21 +1300

    Implement moonbeam call processor

--- packages/contract-processors/src/moonbeam.spec.ts
+++ packages/contract-processors/src/moonbeam.spec.ts
@@ -1,7 +1,8 @@
 // Copyright 2020-2021 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import MoonbeamDatasourcePlugin from './moonbeam';
+import {BigNumber} from '@ethersproject/bignumber';
+import MoonbeamDatasourcePlugin, {substrateHashToEthHash, MoonbeamCall, MoonbeamEvent} from './moonbeam';
 
 describe('MoonbeamDs', () => {
   describe('MoonbeamEvent', () => {
@@ -49,7 +50,7 @@ describe('MoonbeamDs', () => {
     });
 
     describe('Filter Processor', () => {
-      const log = {
+      const log: MoonbeamEvent = {
         address: '0x6bd193ee6d2104f14f94e2ca6efefae561a4334b',
         topics: [
           '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
@@ -152,6 +153,85 @@ describe('MoonbeamDs', () => {
           )
         ).toBeTruthy();
       });
+    });
+  });
+
+  describe('MoonbeamCall', () => {
+    const processor = MoonbeamDatasourcePlugin.handlerProcessors['substrate/MoonbeamCall'];
+
+    describe('Filter Processor', () => {
+      const transaction: MoonbeamCall = {
+        from: '0x0a3f21A6B1B93f15F0d9Dbf0685e3dFdC4889EB0',
+        to: '0xAA30eF758139ae4a7f798112902Bf6d65612045f',
+        data: '0x7ff36ab5000000000000000000000000000000000000000000000000000000003f71d93b00000000000000000000000000000000000000000000000000000000000000800000000000000000000000000a3f21a6b1b93f15f0d9dbf0685e3dfdc4889eb00000000000000000000000000000000000000000000000000000000061708a44000000000000000000000000000000000000000000000000000000000000000200000000000000000000000098878b06940ae243284ca214f92bb71a2b032b8a000000000000000000000000e3f5a90f9cb311505cd691a46596599aa1a0ad7d',
+        value: BigNumber.from(0),
+        nonce: 3378,
+        hash: '0xd2e478f9159967a2c4e70c28d07a62126165ea39127a579aaf18f17d58de180c',
+        blockNumber: 763971,
+        blockHash: '0xfe3bd9d990b3320afdc61e2e78e06801c1adcb20c308825994804c5fa5a283a9',
+        timestamp: Math.round(new Date().getTime() / 1000),
+        gasPrice: BigNumber.from('2875000000'),
+        gasLimit: BigNumber.from(300000),
+        confirmations: 0,
+        chainId: 0,
+      };
+
+      it('can filter from', () => {
+        expect(
+          processor.filterProcessor({from: '0x0a3f21A6B1B93f15F0d9Dbf0685e3dFdC4889EB0'}, transaction, undefined)
+        ).toBeTruthy();
+        expect(
+          processor.filterProcessor({from: '0x0000000000000000000000000000000000000000'}, transaction, undefined)
+        ).toBeFalsy();
+      });
+
+      it('can filter to', () => {
+        expect(
+          processor.filterProcessor({to: '0xAA30eF758139ae4a7f798112902Bf6d65612045f'}, transaction, undefined)
+        ).toBeTruthy();
+        expect(
+          processor.filterProcessor({to: '0x0000000000000000000000000000000000000000'}, transaction, undefined)
+        ).toBeFalsy();
+      });
+
+      it('can filter for contract creation', () => {
+        const contractTx = {...transaction};
+        delete contractTx.to;
+        expect(processor.filterProcessor({to: null}, contractTx, undefined)).toBeTruthy();
+        expect(processor.filterProcessor({to: null}, {...contractTx, to: undefined}, undefined)).toBeTruthy();
+      });
+
+      it('can filter methodId', () => {
+        expect(
+          processor.filterProcessor(
+            {methodId: '0x7ff36ab500000000000000000000000000000000000000000000000000000000'},
+            transaction,
+            undefined
+          )
+        ).toBeTruthy();
+        expect(processor.filterProcessor({methodId: '0x7ff36ab5'}, transaction, undefined)).toBeTruthy();
+
+        expect(processor.filterProcessor({methodId: '0x0000000'}, transaction, undefined)).toBeFalsy();
+      });
+    });
+  });
+
+  describe('Moonbeam utils', () => {
+    it('converts substrate block hash to eth block hash correctly', () => {
+      expect(substrateHashToEthHash('0x0f302f7bcfd6fac512cdfe623ef9b141becc98be0febcee94dc1f8c37a66f3f0')).toEqual(
+        '0x2ddc48977ab437df79ed1df813125d3654e192f1fa3bc997e5f90c80f64d7d91'
+      );
+    });
+
+    // it('converts substrate address to eth address correctly', () => {
+    //   expect(substrateHashToEthHash('0x0f302f7bcfd6fac512cdfe623ef9b141becc98be0febcee94dc1f8c37a66f3f0'))
+    //     .toEqual('0x94256DBc303739E20B68C2A49EDaE812b11a67bC')
+    // });
+
+    it('converts substrate transaction hash to eth transaction hash correctly', () => {
+      expect(substrateHashToEthHash('0xaa84c9aa82d09bb9c969a16e4cd7de014bc737380a5c1bea6f7bf19d863ebb8b')).toEqual(
+        '0x3a829a14031a74a4b3e212c26247d8d8e6599c9a9f927196e90ffce266402954'
+      );
     });
   });
 });
--- packages/contract-processors/src/moonbeam.ts
+++ packages/contract-processors/src/moonbeam.ts
@@ -1,7 +1,8 @@
 // Copyright 2020-2021 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import {Log} from '@ethersproject/abstract-provider';
+import {Log, TransactionResponse} from '@ethersproject/abstract-provider';
+import {BigNumber} from '@ethersproject/bignumber';
 import {isHexString, hexStripZeros} from '@ethersproject/bytes';
 import {ApiPromise} from '@polkadot/api';
 import {CodecHash} from '@polkadot/types/interfaces';
@@ -13,15 +14,17 @@ import {
   SubstrateEvent,
   SubqlEventFilter,
   SecondLayerHandlerProcessor,
+  SubstrateExtrinsic,
 } from '@subql/types';
 import {plainToClass} from 'class-transformer';
 import {
   IsOptional,
-  IsString,
+  IsHexadecimal,
   validateSync,
   ValidatorConstraint,
   ValidatorConstraintInterface,
   Validate,
+  IsEthereumAddress,
 } from 'class-validator';
 
 type TopicFilter = string | string[] | null | undefined;
@@ -33,7 +36,14 @@ export interface MoonbeamEventFilter {
   topics?: [TopicFilter, TopicFilter, TopicFilter, TopicFilter];
 }
 
+export interface MoonbeamCallFilter {
+  from?: string;
+  to?: string;
+  methodId?: string;
+}
+
 export type MoonbeamEvent = Log;
+export type MoonbeamCall = Omit<TransactionResponse, 'wait'>;
 
 @ValidatorConstraint({name: 'topifFilterValidator', async: false})
 export class TopicFilterValidator implements ValidatorConstraintInterface {
@@ -55,11 +65,23 @@ export class TopicFilterValidator implements ValidatorConstraintInterface {
 
 class MoonbeamEventFilterImpl implements MoonbeamEventFilter {
   @IsOptional()
-  @IsString()
+  @IsEthereumAddress()
   address?: string;
   @IsOptional()
   @Validate(TopicFilterValidator, {each: true})
   topics?: [TopicFilter, TopicFilter, TopicFilter, TopicFilter];
+}
+
+class MoonbeamCallFilterImpl implements MoonbeamCallFilter {
+  @IsOptional()
+  @IsEthereumAddress()
+  from?: string;
+  @IsOptional()
+  @IsEthereumAddress()
+  to?: string;
+  @IsOptional()
+  @IsHexadecimal()
+  methodId?: string;
 }
 
 type RawEvent = {
@@ -68,9 +90,34 @@ type RawEvent = {
   data: string;
 };
 
-function polkadotHashToEthHash(original: CodecHash): string {
+type RawTransaction = {
+  nonce: number;
+  gasPrice: string;
+  gasLimit: string;
+  action: {
+    call: string; // hex string
+  };
+  value: string;
+  input: string; // hex string
+  signature: {
+    v: number;
+    r: string; // hex string
+    s: string; // hex string
+  };
+};
+
+type ExecutionEvent = {
+  from: string;
+  to?: string; // Can be undefined for contract creation
+  hash: string;
+  status: unknown;
+};
+
+export function substrateHashToEthHash(original: CodecHash | string): string {
+  const originalStr = typeof original === 'string' ? original : original.toHex();
+
   // TODO something like this https://github.com/PureStake/moonbeam/blob/master/client/rpc/txpool/src/lib.rs#L87
-  return original.toHex();
+  return originalStr;
 }
 
 function hexStringEq(a: string, b: string): boolean {
@@ -80,25 +127,46 @@ function hexStringEq(a: string, b: string): boolean {
   return hexStripZeros(a) === hexStripZeros(b);
 }
 
+function getExecutionEvent(extrinsic: SubstrateExtrinsic): ExecutionEvent {
+  const executionEvent = extrinsic.events.find(
+    (evt) => evt.event.section === 'ethereum' && evt.event.method === 'Executed'
+  );
+
+  if (!executionEvent) {
+    throw new Error('Extrinsic is not an etheruem transaction');
+  }
+
+  const [from, to, hash, status] = executionEvent.event.data;
+
+  return {
+    from: from.toHex(),
+    to: to.toHex(),
+    hash: hash.toHex(),
+    status,
+  };
+}
+
 const EventProcessor: SecondLayerHandlerProcessor<SubqlHandlerKind.Event, MoonbeamEventFilter, MoonbeamEvent> = {
   baseFilter: [{module: 'evm', method: 'Log'}],
   baseHandlerKind: 'substrate/EventHandler' as any /*SubqlHandlerKind.Event*/,
-  transformer(original: SubstrateEvent, ds: MoonbeamDatasource): Log {
+  transformer(original: SubstrateEvent): Log {
     const [eventData] = original.event.data;
+
+    const {hash} = getExecutionEvent(original.extrinsic);
 
     const log = {
       ...(eventData.toJSON() as unknown as RawEvent),
       blockNumber: original.block.block.header.number.toNumber(),
-      blockHash: polkadotHashToEthHash(original.block.hash), // EXPECT 0x2ddc48977ab437df79ed1df813125d3654e192f1fa3bc997e5f90c80f64d7d91
-      transactionIndex: original.extrinsic?.idx, // EXPECT 0
-      transactionHash: polkadotHashToEthHash(original.extrinsic?.extrinsic.hash), // EXPECT 0x3a829a14031a74a4b3e212c26247d8d8e6599c9a9f927196e90ffce266402954
+      blockHash: substrateHashToEthHash(original.block.hash), // EXPECT 0x2ddc48977ab437df79ed1df813125d3654e192f1fa3bc997e5f90c80f64d7d91
+      transactionIndex: original.extrinsic?.idx ?? -1,
+      transactionHash: hash,
       removed: false, // TODO what does this mean?
       logIndex: original.idx, // Might be index of block not index relevant to tx
     };
 
     return log;
   },
-  filterProcessor(filter: MoonbeamEventFilter, input: MoonbeamEvent, ds: MoonbeamDatasource): boolean {
+  filterProcessor(filter: MoonbeamEventFilter, input: MoonbeamEvent): boolean {
     if (filter.address && filter.address !== input.address) {
       return false;
     }
@@ -127,7 +195,66 @@ const EventProcessor: SecondLayerHandlerProcessor<SubqlHandlerKind.Event, Moonbe
 
     if (errors?.length) {
       const errorMsgs = errors.map((e) => e.toString()).join('\n');
-      throw new Error(`Invalid event filter.\n${errorMsgs}`);
+      throw new Error(`Invalid Moonbeam event filter.\n${errorMsgs}`);
+    }
+  },
+};
+
+const CallProcessor: SecondLayerHandlerProcessor<SubqlHandlerKind.Call, MoonbeamCallFilter, MoonbeamCall> = {
+  baseFilter: [{module: 'ethereum', method: 'transact'}],
+  baseHandlerKind: 'substrate/CallHandler' as any /*SubqlHandlerKind.Call*/,
+  transformer(original: SubstrateExtrinsic): MoonbeamCall {
+    const [tx] = original.extrinsic.method.args;
+    const rawTx = tx.toJSON() as unknown as RawTransaction;
+
+    const {from, hash, to} = getExecutionEvent(original);
+
+    const call: MoonbeamCall = {
+      // Transaction properties
+      from,
+      to,
+      nonce: rawTx.nonce,
+      gasLimit: BigNumber.from(rawTx.gasLimit),
+      gasPrice: BigNumber.from(rawTx.gasPrice),
+      data: rawTx.input,
+      value: BigNumber.from(rawTx.value),
+      chainId: undefined, // TODO
+      ...rawTx.signature,
+
+      // Transaction response properties
+      hash,
+      blockNumber: original.block.block.header.number.toNumber(),
+      blockHash: substrateHashToEthHash(original.block.hash),
+      timestamp: Math.round(original.block.timestamp.getTime() / 1000),
+      confirmations: 0, // TODO
+    };
+
+    return call;
+  },
+  filterProcessor(filter: MoonbeamCallFilter, input: MoonbeamCall): boolean {
+    if (filter.from && filter.from !== input.from) {
+      return false;
+    }
+
+    // if `to` is null then we handle contract creation
+    if ((filter.to && filter.to !== input.to) || (filter.to === null && !!input.to)) {
+      return false;
+    }
+
+    if (filter.methodId && input.data.indexOf(filter.methodId) !== 0) {
+      return false;
+    }
+
+    return true;
+  },
+  filterValidator(filter?: MoonbeamCallFilter): void {
+    if (!filter) return;
+    const filterCls = plainToClass(MoonbeamCallFilterImpl, filter);
+    const errors = validateSync(filterCls, {whitelist: true, forbidNonWhitelisted: true});
+
+    if (errors?.length) {
+      const errorMsgs = errors.map((e) => e.toString()).join('\n');
+      throw new Error(`Invalid Moonbeam call filter.\n${errorMsgs}`);
     }
   },
 };
@@ -140,10 +267,11 @@ export const MoonbeamDatasourcePlugin: SubqlDatasourceProcessor<'substrate/Moonb
     return;
   },
   dsFilterProcessor(ds: MoonbeamDatasource, api: ApiPromise): boolean {
-    return true;
+    return ds.kind === this.kind;
   },
   handlerProcessors: {
     'substrate/MoonbeamEvent': EventProcessor,
+    'substrate/MoonbeamCall': CallProcessor,
   },
 };
 
