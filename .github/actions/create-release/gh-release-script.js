d1b45bf68cf1547f9286eb86619060323b0f3c01
Lachlan McCrae <lachlanm@Lachlans-MacBook-Pro.local>
2021-10-25 19:34:32 +1300

    Revert "fix mistakes in release workflow and script"
    This reverts commit 5135086b57c30cd1dc19e6f589df54aaca56bdb2.

--- .github/actions/create-release/gh-release-script.js
+++ .github/actions/create-release/gh-release-script.js
@@ -10,8 +10,6 @@ const pJson = require(`${myArgs[0]}/package.json`)
 const version = pJson.version;
 const repoName = pJson.name; 
 
-const packageName = repoName.split('/');
-
 function checkForBetaVersion(version) {
     if (version.includes('-')){
         exit(0); //skip this package but continue trying to release others
@@ -23,7 +21,6 @@ function gatherReleaseInfo(logPath) {
     const regex = /## \[([0-9]+(\.[0-9]+)+)] - [0-9]{4}-[0-9]{2}-[0-9]{2}/i;
     
     let lines = changeLogs.split(/\n/);
-    let foundChangelog = false;
     let releaseInfo = '';
     let i = 0;
 
@@ -31,24 +28,21 @@ function gatherReleaseInfo(logPath) {
        if(lines[j].includes(`[${version}]`)){
            i = j;
            j = lines.length;
-           foundChangelog = true;
        } 
     }
 
     lines = lines.slice(i);
-
-    if(foundChangelog){
-        for(let j = 0; j < lines.length; j++){
-            if(j == 0){
-               releaseInfo += `${lines[j]}`+ '\n';
-               continue;
-            }        
     
-            if(!regex.test(lines[j])){
-                releaseInfo += `${lines[j]}`+ '\n';
-            } else {
-                j = lines.length;
-            }
+    for(let j = 0; j < lines.length; j++){
+        if(j == 0){
+           releaseInfo += `${lines[j]}`+ '\n';
+           continue;
+        }        
+
+        if(!regex.test(lines[j])){
+            releaseInfo += `${lines[j]}`+ '\n';
+        } else {
+            j = lines.length;
         }
     }
     
@@ -56,11 +50,12 @@ function gatherReleaseInfo(logPath) {
         core.setFailed("No release info found, either missing in changelog or changelog is formatted incorrectly")
     }
 
-    console.info("Gathered release info...")  
+    console.log("Gathered release info...")  
     return releaseInfo;
 }
 
 async function publishRelease(releaseInfo) {
+    const repoTagName = repoName.split('/');
 
     await request('POST /repos/{owner}/{repo}/releases', {
         headers: {
@@ -69,17 +64,17 @@ async function publishRelease(releaseInfo) {
         owner: 'subquery',
         name: `[${version}] ${repoName}`,
         repo: 'subql',
-        tag_name: `${packageName[1]}/${version}`,
+        tag_name: `${repoTagName[1]}/${version}`,
         body: releaseInfo
     }).catch( err => {
         core.setFailed(err)
     })
 
-    console.info("Release Created...")  
+    console.log("Release Created...")  
 }
- 
+
 checkForBetaVersion(version);
 
-const releaseInfo = gatherReleaseInfo(`./packages/${packageName[1]}/CHANGELOG.md`);
+const releaseInfo = gatherReleaseInfo(`./packages/common/CHANGELOG.md`);
 
 publishRelease(releaseInfo);
