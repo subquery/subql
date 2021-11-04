18350f000bd204423f63229dafebf7bf877cca08
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2021-11-05 10:16:24 +1300

    add on interval store lastProcessedHeight

--- packages/node/src/meta/meta.service.ts
+++ packages/node/src/meta/meta.service.ts
@@ -54,11 +54,15 @@ export class MetaService {
   }
 
   @Interval(UPDATE_HEIGHT_INTERVAL)
-  async checkTargetHeight() {
+  async checkHeight() {
     await Promise.all([
       this.storeService.setMetadata(
         'lastProcessedHeight',
         this.lastProcessedHeight,
+      ),
+      this.storeService.setMetadata(
+        'lastProcessedTimestamp',
+        this.lastProcessedTimestamp,
       ),
       this.storeService.setMetadata('targetHeight', this.targetHeight),
     ]);
