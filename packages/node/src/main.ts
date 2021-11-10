760d371f8bc47679ac1392f962496625868f6211
wqsz7xn <wqsz7xn@protonmail.com>
2021-11-10 13:38:37 +1300

    force-clean

--- packages/node/src/main.ts
+++ packages/node/src/main.ts
@@ -2,24 +2,70 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { NestFactory } from '@nestjs/core';
+import { QueryTypes, Sequelize } from 'sequelize';
 import { AppModule } from './app.module';
+import { NodeConfig } from './configure/NodeConfig';
 import { IndexerManager } from './indexer/indexer.manager';
 import { getLogger, NestLogger } from './utils/logger';
 import { argv } from './yargs';
 
+const logger = getLogger('subql-node');
+
 async function bootstrap() {
+  const forceclean = argv('force-clean');
   const debug = argv('debug');
+
   try {
     const app = await NestFactory.create(AppModule, {
       logger: debug ? new NestLogger() : false,
     });
     await app.init();
+
+    if (forceclean) {
+      const sequelize = app.get(Sequelize);
+      const nodeConfig = app.get(NodeConfig);
+      sequelize.authenticate();
+
+      try {
+        // map subquery project name to db schema
+        const [{ db_schema: subquerySchema }] = await sequelize.query(
+          `SELECT db_schema
+        FROM public.subqueries
+        where name = '${nodeConfig.subqueryName}'`,
+          {
+            type: QueryTypes.SELECT,
+          },
+        );
+
+        sequelize.dropSchema(subquerySchema, {
+          logging: false,
+          benchmark: false,
+        });
+
+        // remove db schema from project table
+        await sequelize.query(
+          ` DELETE
+          FROM public.subqueries
+          where db_schema = '${subquerySchema}'`,
+          {
+            type: QueryTypes.DELETE,
+          },
+        );
+      } catch (err) {
+        logger.error(err, 'failed to force clean tables');
+      }
+
+      logger.info('force cleaned tables');
+    }
+
     const indexerManager = app.get(IndexerManager);
     await indexerManager.start();
+
     await app.listen(3000);
-    getLogger('subql-node').info('node started');
+    logger.info('node started');
+    logger.info('listening on http://localhost:3000/');
   } catch (e) {
-    getLogger('subql-node').error(e, 'node failed to start');
+    logger.error(e, 'node failed to start');
     process.exit(1);
   }
 }
