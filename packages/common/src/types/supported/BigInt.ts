67b6fbabd6eced3d140a299e0f5ba6c93deb6b36
JQQQ <jiqiang90@hotmail.com>
2021-11-14 11:56:05 +1300

    change BN to bn

--- packages/common/src/types/supported/BigInt.ts
+++ packages/common/src/types/supported/BigInt.ts
@@ -1,7 +1,7 @@
 // Copyright 2020-2021 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import BN from 'BN.js';
+import BN from 'bn.js';
 import {TypeClass} from '../TypeClass';
 
 export const BigInt = new TypeClass(
