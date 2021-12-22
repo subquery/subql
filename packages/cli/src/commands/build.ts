3d76671cc77968e06bca6e04cc3c06f12d669128
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2021-12-22 17:03:19 +1300

    update runWebpack command

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -35,10 +35,12 @@ export default class Build extends Command {
 
     // Get the output location from the project package.json main field
     const pjson = JSON.parse(readFileSync(path.join(directory, 'package.json')).toString());
+
+    const buildPath = typeof pjson.exports === 'string' ? pjson.exports : 'src/index.ts';
     const outputPath = path.resolve(directory, pjson.main || 'dist/index.js');
 
     cli.action.start('Building and packing code');
-    await runWebpack(path.join(directory, 'src/index.ts'), outputPath, isDev, true);
+    await runWebpack(path.join(directory, buildPath), outputPath, isDev, true);
     cli.action.stop();
   }
 }
