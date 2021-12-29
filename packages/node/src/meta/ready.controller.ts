65a66bffb28949b3baa1d438fc6b5389a5d14673
wqsz7xn <wqsz7xn@protonmail.com>
2021-12-30 11:08:42 +1300

    ready endpoint and apk curl docker node

---
+++ packages/node/src/meta/ready.controller.ts
@@ -0,0 +1,18 @@
+// Copyright 2020-2021 OnFinality Limited authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { Controller, Get } from '@nestjs/common';
+import { getLogger } from '../utils/logger';
+import { ReadyService } from './ready.service';
+
+const logger = getLogger('ready');
+
+@Controller('ready')
+export class ReadyController {
+  constructor(private readyService: ReadyService) {}
+
+  @Get()
+  getReady() {
+    return { ready: this.readyService.getReady };
+  }
+}
