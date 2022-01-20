620273e9f55ed29ecc7f6da91f570b22a020d371
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2022-01-21 10:11:52 +1300

    update interface imports

--- packages/query/src/graphql/plugins/PgAggregationPlugin.ts
+++ packages/query/src/graphql/plugins/PgAggregationPlugin.ts
@@ -10,64 +10,15 @@ import AddHavingAggregateTypesPlugin from '@graphile/pg-aggregates/dist/AddHavin
 import AggregateSpecsPlugin from '@graphile/pg-aggregates/dist/AggregateSpecsPlugin';
 import FilterRelationalAggregatesPlugin from '@graphile/pg-aggregates/dist/FilterRelationalAggregatesPlugin';
 import InflectionPlugin from '@graphile/pg-aggregates/dist/InflectionPlugin';
+import {AggregateSpec, AggregateGroupBySpec} from '@graphile/pg-aggregates/dist/interfaces';
 import OrderByAggregatesPlugin from '@graphile/pg-aggregates/dist/OrderByAggregatesPlugin';
 
 import type {Plugin} from 'graphile-build';
-import type {PgAttribute, PgType, SQL, PgProc} from 'graphile-build-pg';
 import {makePluginByCombiningPlugins} from 'graphile-utils';
 
 import {argv} from '../../yargs';
 
 const unsafe = argv('unsafe') as boolean;
-
-interface AggregateGroupBySpec {
-  /** Must not change since it's used in type names/etc */
-  id: string; // e.g. 'truncated-to-hour'
-
-  /** Return true if we can process this type */
-  isSuitableType: (type: PgType) => boolean;
-
-  /** Return false if we cannot process this attribute (default: true) */
-  shouldApplyToEntity?: (entity: PgAttribute | PgProc) => boolean;
-
-  /** Wraps the SQL to return a derivative (e.g. sqlFrag => sql.fragment`date_trunc('hour', ${sqlFrag})`) */
-  sqlWrap: (sqlFrag: SQL) => SQL;
-}
-
-interface AggregateSpec {
-  /** Must not change since it's used in type names/etc */
-  id: string;
-
-  /** Used in descriptions, starts with lowercase */
-  humanLabel: string;
-
-  /** Used in descriptions, starts with uppercase */
-  HumanLabel: string;
-
-  /** Return true if we can process this type */
-  isSuitableType: (type: PgType) => boolean;
-
-  /** Return false if we cannot process this attribute (default: true) */
-  shouldApplyToEntity?: (entity: PgAttribute | PgProc) => boolean;
-
-  /** Wraps the SQL in an aggregate call */
-  sqlAggregateWrap: (sqlFrag: SQL) => SQL;
-
-  /**
-   * Used to translate the PostgreSQL return type for the aggregate; for example:
-   *
-   * - Sum over int should give bigint
-   * - Average of int should be float
-   * - Median of int should be int
-   */
-  pgTypeAndModifierModifier?: (
-    pgType: PgType,
-    pgTypeModifier: null | string | number
-  ) => [PgType, null | string | number];
-
-  /** Set true if the result is guaranteed to be non-null */
-  isNonNull?: boolean;
-}
 
 // overwrite the official plugin: https://github.com/graphile/pg-aggregates/blob/main/src/AggregateSpecsPlugin.ts
 // Removes all aggregation functions when not using --unsafe flag.
