4628b2940d633fb89a944906cea31515a6dad7e8
JQQQ <jiqiang90@hotmail.com>
2022-02-09 10:41:00 +1300

    Remove secret

--- packages/cli/src/commands/publish.ts
+++ packages/cli/src/commands/publish.ts
@@ -5,15 +5,8 @@ import {lstatSync, readFileSync} from 'fs';
 import * as fs from 'fs';
 import path from 'path';
 import {Command, flags} from '@oclif/command';
-import yaml from 'js-yaml';
 import {uploadToIpfs} from '../controller/publish-controller';
 import Build from './build';
-
-// Authorization spec is small enough not to have its own specVersion.
-// Additionally when authorization requirements change we would like this to be a breaking change
-class AuthorizationSpec {
-  secret: string;
-}
 
 const ACCESS_TOKEN_PATH = path.resolve(process.env.HOME, '.subql/SUBQL_ACCESS_TOKEN');
 
@@ -48,9 +41,7 @@ export default class Publish extends Command {
       authToken = process.env.SUBQL_ACCESS_TOKEN;
     } else if (fs.existsSync(ACCESS_TOKEN_PATH)) {
       try {
-        authToken =
-          process.env.SUBQL_ACCESS_TOKEN ??
-          (yaml.load(fs.readFileSync(ACCESS_TOKEN_PATH, 'utf8')) as AuthorizationSpec).secret;
+        authToken = process.env.SUBQL_ACCESS_TOKEN ?? fs.readFileSync(ACCESS_TOKEN_PATH, 'utf8');
       } catch (e) {
         this.error(`Failed to read SUBQL_ACCESS_TOKEN from ${ACCESS_TOKEN_PATH}: ${e}`);
       }
@@ -59,7 +50,7 @@ export default class Publish extends Command {
     }
 
     this.log('Uploading SupQuery project to IPFS');
-    const cid = await uploadToIpfs(directory, authToken, flags.ipfs).catch((e) => this.error(e));
+    const cid = await uploadToIpfs(directory, authToken.trim(), flags.ipfs).catch((e) => this.error(e));
     this.log(`SubQuery Project uploaded to IPFS: ${cid}`);
   }
 }
