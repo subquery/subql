76928c3407a3ed1d194aa57368f59f9de74bdd6b
JQQQ <jiqiang90@hotmail.com>
2022-03-09 12:30:30 +1300

    Fix test

--- packages/common-substrate/src/project/versioned/ProjectManifestVersioned.ts
+++ packages/common-substrate/src/project/versioned/ProjectManifestVersioned.ts
@@ -51,8 +51,6 @@ export class SubstrateProjectManifestVersioned implements ISubstrateProjectManif
     if (!klass) {
       throw new Error('specVersion not supported for project manifest file');
     }
-    const impl = (this._impl = plainToClass<ProjectManifestImpls, VersionedProjectManifest>(klass, projectManifest));
-
     this._impl = plainToClass<ProjectManifestImpls, VersionedProjectManifest>(klass, projectManifest);
   }
 
