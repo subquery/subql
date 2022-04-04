339d28eed1fdd79175b43c1cbba5366dc672683c
Jay Ji <jiqiang90@hotmail.com>
2022-04-04 18:27:26 +1200

    Add the special resolve function (#873)
    * Add the special resolve function
    
    * fix

--- packages/node-terra/src/indexer/sandboxterra.service.ts
+++ packages/node-terra/src/indexer/sandboxterra.service.ts
@@ -49,6 +49,9 @@ export class Sandbox extends NodeVM {
       merge(DEFAULT_OPTION, {
         require: {
           root: option.root,
+          resolve: (moduleName: string) => {
+            return require.resolve(moduleName, { paths: [option.root] });
+          },
         },
       }),
     );
