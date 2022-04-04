b2a987fb55bbedef3b37ba7d2fa60a87b33f13dd
JQQQ <jiqiang90@hotmail.com>
2022-04-04 18:41:08 +1200

    Revert "Revert "Add the special resolve function""
    This reverts commit d8bf2d9e51c28df6605ddb279f5f07409b110973.

--- packages/node/src/indexer/sandbox.service.ts
+++ packages/node/src/indexer/sandbox.service.ts
@@ -50,6 +50,9 @@ export class Sandbox extends NodeVM {
       merge(DEFAULT_OPTION, {
         require: {
           root: option.root,
+          resolve: (moduleName: string) => {
+            return require.resolve(moduleName, { paths: [option.root] });
+          },
         },
       }),
     );
