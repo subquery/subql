d434e22afd12558c8167c770331b50921d586e4e
roduquen <duquenne.robin42@gmail.com>
2022-05-05 14:20:06 +0200

    removing useless test

--- packages/common-test/src/constants.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2020-2022 OnFinality Limited authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-export const IPFS_DEV = 'https://interipfs.thechaindata.com';
-export const IPFS_PROD = 'https://ipfs.subquery.network';
-export const IPFS_NODE_ENDPOINT = `${IPFS_PROD}/ipfs/api/v0`;
-export const IPFS_CLUSTER_ENDPOINT = `${IPFS_PROD}/cluster/add`;
-export const IPFS_REGEX = /^ipfs:\/\//i;
