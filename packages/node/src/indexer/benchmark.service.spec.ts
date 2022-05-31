86af0fe6f6510e3cbb43f12c47b49dfffa8490f1
bz888 <ziyu.ben158@gmail.com>
2022-05-31 16:30:07 +1200

    added jest.spyon for loggers

--- packages/node/src/indexer/benchmark.service.spec.ts
+++ packages/node/src/indexer/benchmark.service.spec.ts
@@ -1,11 +1,19 @@
 // Copyright 2020-2022 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { getLogger } from '../utils/logger';
 import { delay } from '../utils/promise';
 import { BenchmarkService } from './benchmark.service';
 
 jest.setTimeout(90000);
 describe('Benchmark service', () => {
+  let loggerSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    const logger = getLogger('benchmark');
+    loggerSpy = jest.spyOn(logger, 'info');
+  });
+
   it('Handle bps when fully synced', async () => {
     const newbenchmarkService = new BenchmarkService();
 
@@ -17,6 +25,9 @@ describe('Benchmark service', () => {
 
     await newbenchmarkService.benchmark();
     await delay(20);
+    expect(loggerSpy).toHaveBeenCalledWith(
+      'Fully synced, waiting for new blocks',
+    );
   });
 
   it('Connection dropped', async () => {
@@ -30,6 +41,9 @@ describe('Benchmark service', () => {
 
     await newbenchmarkService.benchmark();
     await delay(20);
+    expect(loggerSpy).toHaveBeenCalledWith(
+      '0.00 bps, target: #1209000, current: #1209000, estimate time: unknown',
+    );
   });
 
   it('Handle normal', async () => {
@@ -43,5 +57,8 @@ describe('Benchmark service', () => {
 
     await newbenchmarkService.benchmark();
     await delay(20);
+    expect(loggerSpy).toHaveBeenCalledWith(
+      '0.20 bps, target: #1209163, current: #1208163, estimate time: 0 days 01 hours 23 mins',
+    );
   });
 });
