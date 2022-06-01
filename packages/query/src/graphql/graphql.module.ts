bb02d19f8b517aca4b780a8d52f6500703fada86
bz888 <ziyu.ben158@gmail.com>
2022-06-01 15:38:33 +1200

    changes made based on comments

--- packages/query/src/graphql/graphql.module.ts
+++ packages/query/src/graphql/graphql.module.ts
@@ -85,10 +85,9 @@ export class GraphqlModule implements OnModuleInit, OnModuleDestroy {
         ? ApolloServerPluginLandingPageGraphQLPlayground()
         : ApolloServerPluginLandingPageDisabled(),
     ];
-    //If query-complexity is set then use plugin, otherwise not add
-    // if (argv['query-complexity'] !== undefined) {
+
     apolloServerPlugins.push(queryComplexityPlugin({schema, maxComplexity: argv['query-complexity']}));
-    // }
+
     const server = new ApolloServer({
       schema,
       context: {
