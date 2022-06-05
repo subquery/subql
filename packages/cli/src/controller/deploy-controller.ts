8437b7d746ebe663fc4354ea1572f4d5411957ba
bz888 <ziyu.ben158@gmail.com>
2022-06-05 20:38:04 +1200

    delete works

--- packages/cli/src/controller/deploy-controller.ts
+++ packages/cli/src/controller/deploy-controller.ts
@@ -14,28 +14,34 @@ export async function deployToHostedService(
   dictEndpoint?: string
 ): Promise<string> {
   try {
-    await axios({
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
-      method: 'post',
-      url: `https://api.thechaindata.com/v2/subqueries/${key}/deployments`,
-      data: {
-        dictEndpoint:
-          dictEndpoint !== undefined ? dictEndpoint : 'https://api.subquery.network/sq/subquery/altair-dictionary',
-        endpoint: endpoint !== undefined ? endpoint : 'wss://polkadot.api.onfinality.io/public-ws',
-        version: ipfsCID,
-        advancedSettings: {
-          '@subql/node': {},
-          '@subql/query': {},
+    const result = (
+      await axios({
+        headers: {
+          Authorization: `Bearer ${authToken}`,
         },
-        indexerImageVersion: indexerImageVersion !== undefined ? indexerImageVersion : 'v1.1.2',
-        queryImageVersion: queryImageVersion !== undefined ? queryImageVersion : 'v1.1.1',
-        type: type !== undefined ? type : 'primary',
-      },
-    });
+        method: 'post',
+        url: `https://api.thechaindata.com/v2/subqueries/${key}/deployments`,
+        data: {
+          dictEndpoint:
+            dictEndpoint !== undefined ? dictEndpoint : 'https://api.subquery.network/sq/subquery/altair-dictionary',
+          endpoint: endpoint !== undefined ? endpoint : 'wss://polkadot.api.onfinality.io/public-ws',
+          version: ipfsCID,
+          advancedSettings: {
+            '@subql/node': {},
+            '@subql/query': {},
+          },
+          indexerImageVersion: indexerImageVersion !== undefined ? indexerImageVersion : 'v1.1.2',
+          queryImageVersion: queryImageVersion !== undefined ? queryImageVersion : 'v1.1.1',
+          type: type !== undefined ? type : 'primary',
+        },
+      })
+    ).data;
+    return `Success, Delployment ID: ${result.deployment.id}`;
   } catch (e) {
     throw new Error(`Failed to deploy to hosted service: ${e.message}`);
   }
-  return 'Success';
 }
+
+// interface DeployResponseData {
+
+// }
