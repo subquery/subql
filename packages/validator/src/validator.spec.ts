33a4f4b888c3d371789ee80983c79bf338c9be7f
JQQQ <jiqiang90@hotmail.com>
2022-06-21 16:53:04 +1200

    add broken tests

--- packages/validator/src/validator.spec.ts
+++ packages/validator/src/validator.spec.ts
@@ -1,17 +1,23 @@
 // Copyright 2020-2022 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import {NETWORK_FAMILY} from '@subql/common';
-import {commonRules} from './rules';
+import {IPFS_NODE_ENDPOINT, NETWORK_FAMILY} from '@subql/common';
+import {commonRules, deploymentRules} from './rules';
 import {Validator} from './validator';
 
-describe('Validator', () => {
+describe('Validate project below manifest spec 1.0.0', () => {
   let v: Validator;
+  const url = 'https://github.com/subquery/tutorials-block-timestamp';
 
   beforeAll(async () => {
-    const url = 'https://github.com/subquery/tutorials-block-timestamp';
     v = await Validator.create(url, null, NETWORK_FAMILY.substrate);
     v.addRule(...commonRules);
+  });
+
+  it('should throw error if manifest is below 1.0.0 and network is not provided', async () => {
+    const v1 = await Validator.create(url);
+    v1.addRule(...commonRules);
+    await expect(v1.getValidateReports()).rejects.toThrow(/Can not identify project network under spec version 1.0.0/);
   });
 
   it('should validate get reports', async () => {
@@ -22,5 +28,23 @@ describe('Validator', () => {
   it('should return validate result', async () => {
     const result = await v.validate();
     expect(result).toBeTruthy();
+  });
+});
+
+describe('Validate project with manifest spec 1.0.0, auto identify network', () => {
+  it('should validate IPFS deployment', async () => {
+    const cid = 'ipfs://QmVX1aaJRL9f5Vgjr8VJ9MWAcFHzAya9omnQ534JYCigCQ';
+    const v = await Validator.create(cid, {ipfs: IPFS_NODE_ENDPOINT});
+    v.addRule(...deploymentRules);
+    const result = await v.getValidateReports();
+    expect(result.filter((r) => r.valid).length).toBe(result.length);
+  });
+
+  it('should validate get reports', async () => {
+    const url = 'https://github.com/subquery/juno-subql-dictionary';
+    const v = await Validator.create(url);
+    v.addRule(...commonRules);
+    const result = await v.getValidateReports();
+    expect(result.filter((r) => r.valid).length).toBe(result.length);
   });
 });
