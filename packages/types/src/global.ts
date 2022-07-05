119da6c2b6f5432022fb89699e1f1d28ee96f2f2
Scott Twiname <skott.twiname@gmail.com>
2022-07-05 14:14:19 +1200

    Add cosmos deps to types (#36)

--- packages/types/src/global.ts
+++ packages/types/src/global.ts
@@ -1,10 +1,9 @@
 // Copyright 2020-2022 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import {CosmWasmClient} from '@cosmjs/cosmwasm-stargate';
-import {Registry} from '@cosmjs/proto-signing';
-import {StargateClient} from '@cosmjs/stargate';
-import Pino from 'pino';
+import type {CosmWasmClient} from '@cosmjs/cosmwasm-stargate';
+import type {Registry} from '@cosmjs/proto-signing';
+import type Pino from 'pino';
 import {Store, DynamicDatasourceCreator} from './interfaces';
 
 declare global {
