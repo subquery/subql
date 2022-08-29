f38d9a78c13218cf48334a5903ebc2e5247fb03d
DeveloperInProgress <velnaveen99@gmail.com>
2022-08-29 07:14:58 +0000

    lint fixes

--- packages/types-avalanche/src/ethereum/interfaces.ts
+++ packages/types-avalanche/src/ethereum/interfaces.ts
@@ -1,27 +1,26 @@
 // Copyright 2020-2022 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import {ethers} from 'ethers';
+import {AvalancheBlockFilter, AvalancheTransactionFilter, AvalancheLogFilter, AvalancheResult} from '../avalanche';
 import {BlockWrapper} from '../interfaces';
-import { AvalancheBlockFilter, AvalancheTransactionFilter, AvalancheLogFilter, AvalancheResult } from '../avalanche';
-import { ethers } from 'ethers';
 
 export type EthereumResult = AvalancheResult;
 
 export type EthereumBlock = ethers.providers.Block;
 
 export type EthereumTransaction = ethers.providers.TransactionResponse & {
-    receipt: ethers.providers.TransactionReceipt;
-}
+  receipt: ethers.providers.TransactionReceipt;
+};
 
 export type EthereumLog<T extends EthereumResult = EthereumResult> = ethers.providers.Log & {
-    args?: T;
-}
+  args?: T;
+};
 
 export type EthereumBlockWrapper = BlockWrapper<
-    EthereumBlock,
-    EthereumTransaction,
-    EthereumLog,
-    AvalancheTransactionFilter,
-    AvalancheLogFilter
+  EthereumBlock,
+  EthereumTransaction,
+  EthereumLog,
+  AvalancheTransactionFilter,
+  AvalancheLogFilter
 >;
-
