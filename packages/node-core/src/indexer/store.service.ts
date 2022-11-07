5e96a41df01bc067aac22fe711682d1c923d716f
Jay Ji <jiqiang90@hotmail.com>
2022-11-08 11:24:21 +1300

    Fix remove alter table (#1387)
    * remove migrate alter table
    
    * remove

--- packages/node-core/src/indexer/store.service.ts
+++ packages/node-core/src/indexer/store.service.ts
@@ -259,7 +259,6 @@ export class StoreService {
     // this will allow alter current entity, including fields
     // TODO, add rules for changes, eg only allow add nullable field
     // Only allow altering the tables on the main thread
-    await this.sequelize.sync({alter: {drop: isMainThread}});
     await this.setMetadata('historicalStateEnabled', this.historical);
     for (const query of extraQueries) {
       await this.sequelize.query(query);
