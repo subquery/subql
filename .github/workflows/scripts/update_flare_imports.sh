68c8235f8fa519729500256b80320a203cebba1b
Naveen V <velnaveen99@gmail.com>
2022-11-17 01:26:57 +0530

    Fix update_flare_imports script (#13)
    * allow flare datasource and handler kinds
    
    * add ethereum handlers and datasources
    
    * rename FlareHandlerKind to Ethereum
    
    * update CI and docker
    
    * update imports
    
    * revert and add flare publish workflow
    
    * revert package names after CI run
    
    * update node-docker workflow to publish node-flare
    
    * add flare types
    
    * create seperate workflow for flare
    
    * fix update_flare_imports script
    
    * workflow cleanup
    
    * remove workflow-dispatch in prerelease

--- .github/workflows/scripts/update_flare_imports.sh
+++ .github/workflows/scripts/update_flare_imports.sh
@@ -4,16 +4,10 @@ replace_paths() {
     file="${1}"
     sed -i "s/@subql\/common-ethereum/@subql\/common-flare/g" ${file}
     sed -i "s/@subql\/types-ethereum/@subql\/types-flare/g" ${file}
+    sed -i "s/@subql\/node-ethereum/@subql\/node-flare/g" ${file}
 }
 
-for file in packages/**/*.ts;
-do
-    if [ -f "${file}" ]; then
-        replace_paths $file
-    fi
-done
-
-for file in packages/**/package.json;
+for file in $(find ./packages -name '*.ts' -or -name 'package.json');
 do
     if [ -f "${file}" ]; then
         replace_paths $file
