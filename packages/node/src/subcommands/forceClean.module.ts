af11f5e0e45152afb6812b37be734c35b4cec746
JQQQ <jiqiang90@hotmail.com>
2022-11-24 10:38:24 +1300

    tidy up

--- packages/node/src/subcommands/forceClean.module.ts
+++ packages/node/src/subcommands/forceClean.module.ts
@@ -2,24 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Module } from '@nestjs/common';
-import { DbModule, NodeConfig } from '@subql/node-core';
-import { Sequelize } from 'sequelize';
+import { DbModule } from '@subql/node-core';
 import { ConfigureModule } from '../configure/configure.module';
-import { SubqueryProject } from '../configure/SubqueryProject';
 import { ForceCleanService } from './forceClean.service';
 
 @Module({
-  providers: [
-    ForceCleanService,
-    // {
-    //   provide: ForceCleanService,
-    //   useFactory: (sequelize:Sequelize, nodeConfig: NodeConfig,project: SubqueryProject) => {
-    //     const forceCleanService = new ForceCleanService(sequelize, nodeConfig,project);
-    //     return forceCleanService;
-    //   },
-    //   inject: ['ISubqueryProject', NodeConfig,Sequelize],
-    // }
-  ],
+  providers: [ForceCleanService],
   controllers: [],
 })
 export class ForceCleanFeatureModule {}
