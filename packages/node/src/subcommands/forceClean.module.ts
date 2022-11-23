d0e1c0ae0a0023e78da87d390bdd7a9563936790
JQQQ <jiqiang90@hotmail.com>
2022-11-24 10:36:40 +1300

    update polkadot api, also fix force-clean dependencies issue

--- packages/node/src/subcommands/forceClean.module.ts
+++ packages/node/src/subcommands/forceClean.module.ts
@@ -2,12 +2,24 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Module } from '@nestjs/common';
-import { DbModule } from '@subql/node-core';
+import { DbModule, NodeConfig } from '@subql/node-core';
+import { Sequelize } from 'sequelize';
 import { ConfigureModule } from '../configure/configure.module';
+import { SubqueryProject } from '../configure/SubqueryProject';
 import { ForceCleanService } from './forceClean.service';
 
 @Module({
-  providers: [ForceCleanService],
+  providers: [
+    ForceCleanService,
+    // {
+    //   provide: ForceCleanService,
+    //   useFactory: (sequelize:Sequelize, nodeConfig: NodeConfig,project: SubqueryProject) => {
+    //     const forceCleanService = new ForceCleanService(sequelize, nodeConfig,project);
+    //     return forceCleanService;
+    //   },
+    //   inject: ['ISubqueryProject', NodeConfig,Sequelize],
+    // }
+  ],
   controllers: [],
 })
 export class ForceCleanFeatureModule {}
