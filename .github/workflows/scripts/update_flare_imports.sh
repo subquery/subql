bf171acafb91b84add68997dcd51dd20b27889d8
Scott Twiname <skott.twiname@gmail.com>
2022-11-24 17:02:42 +1300

    Fix bin name

--- .github/workflows/scripts/update_flare_imports.sh
+++ .github/workflows/scripts/update_flare_imports.sh
@@ -7,6 +7,7 @@ replace_paths() {
     sed -i "s/@subql\/common-ethereum/@subql\/common-flare/g" ${file}
     sed -i "s/@subql\/types-ethereum/@subql\/types-flare/g" ${file}
     sed -i "s/@subql\/node-ethereum/@subql\/node-flare/g" ${file}
+    sed -i "s/subql-node-ethereum/subql-node-flare/g" ${file}
 }
 
 for file in $(find ./packages -name '*.ts' -or -name 'package.json');
