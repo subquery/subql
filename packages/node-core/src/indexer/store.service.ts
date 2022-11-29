9bc36a39fc570e7ce14dcb03e94b514f320f902b
Jay Ji <jiqiang90@hotmail.com>
2022-11-29 22:25:25 +1300

    fix enum name missing issue (#1441)
    * fix enum name missing issue
    
    * tidy up

--- packages/node-core/src/indexer/store.service.ts
+++ packages/node-core/src/indexer/store.service.ts
@@ -159,16 +159,17 @@ export class StoreService {
           );
         }
       }
+      // Ref: https://www.graphile.org/postgraphile/enums/
+      // Example query for enum name: COMMENT ON TYPE "polkadot-starter_enum_a40fe73329" IS E'@enum\n@enumName TestEnum'
+      // It is difficult for sequelize use replacement, instead we use escape to avoid injection
 
-      const comment = `@enum\\n@enumName ${e.name}${e.description ? `\\n ${e.description}` : ''}`;
-
-      await this.sequelize.query(`COMMENT ON TYPE "${enumTypeName}" IS ?`, {
-        replacements: [comment],
-      });
+      const comment = this.sequelize.escape(
+        `@enum\\n@enumName ${e.name}${e.description ? `\\n ${e.description}` : ''}`
+      );
+      await this.sequelize.query(`COMMENT ON TYPE "${enumTypeName}" IS E${comment}`);
       enumTypeMap.set(e.name, `"${enumTypeName}"`);
     }
     const extraQueries = [];
-
     if (this.config.subscription) {
       extraQueries.push(createSendNotificationTriggerFunction);
     }
