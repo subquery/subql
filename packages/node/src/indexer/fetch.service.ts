deb51ce345da6261fd0ebbdd521ddeb18c3786cb
bz888 <ziyu.ben158@gmail.com>
2022-12-01 09:43:10 +1300

    add comments

--- packages/node/src/indexer/fetch.service.ts
+++ packages/node/src/indexer/fetch.service.ts
@@ -491,6 +491,9 @@ export class FetchService implements OnApplicationShutdown {
               const cleanedBatchBlocks =
                 this.filteredBlockBatch(enqueuingBlocks);
               this.blockDispatcher.enqueueBlocks(cleanedBatchBlocks);
+
+              // In the case where factors of batchSize is equal to bypassBlock or when cleanedBatchBlocks is []
+              // to ensure block is bypassed, latestBufferHeight is needs to manually set
               this.setLatestBufferHeight(cleanedBatchBlocks, enqueuingBlocks);
             }
             continue; // skip nextBlockRange() way
