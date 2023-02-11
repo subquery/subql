cb01202206f97bce93ed5fb80b27ca9ead0379f9
Darwin Subramaniam <darwinsubramaniam@googlemail.com>
2023-02-11 17:27:04 +0800

    adding SSL option for query pacakge.

--- packages/query/src/configure/configure.module.ts
+++ packages/query/src/configure/configure.module.ts
@@ -1,6 +1,8 @@
 // Copyright 2020-2022 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import {readFileSync} from 'fs';
+import {ConnectionOptions} from 'tls';
 import {DynamicModule, Global, Module} from '@nestjs/common';
 import {Pool} from 'pg';
 import {getLogger} from '../utils/logger';
@@ -20,6 +22,18 @@ export class ConfigureModule {
       unsafe: opts.unsafe ?? false,
     });
 
+    const sslOption = (_config: Config) => {
+      if (_config.get('DB_SSL')) {
+        const sslConfig: ConnectionOptions = {
+          ca: readFileSync(_config.get('DB_CA_CERT')).toString(),
+          key: _config.get('DB_KEY_CERT') ? readFileSync(_config.get('DB_KEY_CERT')).toString() : undefined,
+          cert: _config.get('DB_CLIENT_CERT') ? readFileSync(_config.get('DB_CLIENT_CERT')).toString() : undefined,
+        };
+        return sslConfig;
+      }
+      return false;
+    };
+
     const pgPool = new Pool({
       user: config.get('DB_USER'),
       password: config.get('DB_PASS'),
@@ -28,6 +42,7 @@ export class ConfigureModule {
       database: config.get('DB_DATABASE'),
       max: opts['max-connection'],
       statement_timeout: opts['query-timeout'],
+      ssl: sslOption(config),
     });
     pgPool.on('error', (err) => {
       // tslint:disable-next-line no-console
