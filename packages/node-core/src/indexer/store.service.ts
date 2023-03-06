1222bf90ab4135607e769ca8eecf8aab8a554693
JQQQ <jiqiang90@hotmail.com>
2023-03-06 13:03:41 +1300

    remove unused

--- packages/node-core/src/indexer/store.service.ts
+++ packages/node-core/src/indexer/store.service.ts
@@ -39,7 +39,6 @@ import {
   camelCaseObjectKey,
   commentConstraintQuery,
   commentTableQuery,
-  createExcludeConstraintQuery,
   createNotifyTrigger,
   createSchemaTrigger,
   createSchemaTriggerFunction,
