109de0f587c252692fa5f2c1f668253644583df2
Scott Twiname <skott.twiname@gmail.com>
2023-05-04 16:01:02 +1200

    Add mutex around cachePoi

--- packages/node-core/src/indexer/storeCache/cachePoi.ts
+++ packages/node-core/src/indexer/storeCache/cachePoi.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import {u8aToBuffer} from '@subql/utils';
+import {Mutex} from 'async-mutex';
 import {Op, Transaction} from 'sequelize';
 import {getLogger} from '../../logger';
 import {PoiRepo, ProofOfIndex} from '../entities';
@@ -15,6 +16,7 @@ export class CachePoiModel implements ICachedModelControl {
   private setCache: Record<number, ProofOfIndex> = {};
   private removeCache: number[] = [];
   flushableRecordCounter = 0;
+  private mutex = new Mutex();
 
   constructor(readonly model: PoiRepo) {}
 
@@ -31,6 +33,7 @@ export class CachePoiModel implements ICachedModelControl {
   }
 
   async getById(id: number): Promise<ProofOfIndex | undefined> {
+    await this.mutex.waitForUnlock();
     if (this.removeCache.includes(id)) {
       logger.debug(`Attempted to get deleted POI with id="${id}"`);
       return undefined;
@@ -52,6 +55,7 @@ export class CachePoiModel implements ICachedModelControl {
   }
 
   async getPoiBlocksByRange(startHeight: number): Promise<ProofOfIndex[]> {
+    await this.mutex.waitForUnlock();
     const result = await this.model.findAll({
       limit: DEFAULT_FETCH_RANGE,
       where: {id: {[Op.gte]: startHeight}},
@@ -69,6 +73,7 @@ export class CachePoiModel implements ICachedModelControl {
   }
 
   async getLatestPoi(): Promise<ProofOfIndex | null | undefined> {
+    await this.mutex.waitForUnlock();
     const result = await this.model.findOne({
       order: [['id', 'DESC']],
     });
@@ -77,6 +82,7 @@ export class CachePoiModel implements ICachedModelControl {
   }
 
   async getLatestPoiWithMmr(): Promise<ProofOfIndex | null> {
+    await this.mutex.waitForUnlock();
     const result = await this.model.findOne({
       order: [['id', 'DESC']],
       where: {mmrRoot: {[Op.ne]: null}} as any, // Types problem with sequelize, undefined works but not null
@@ -90,17 +96,26 @@ export class CachePoiModel implements ICachedModelControl {
   }
 
   async flush(tx: Transaction): Promise<void> {
-    logger.debug(`Flushing ${this.flushableRecordCounter} items from cache`);
-    const pendingFlush = Promise.all([
-      this.model.bulkCreate(Object.values(this.setCache), {transaction: tx, updateOnDuplicate: ['mmrRoot']}),
-      this.model.destroy({where: {id: this.removeCache}, transaction: tx}),
-    ]);
+    const release = await this.mutex.acquire();
+    try {
+      tx.afterCommit(() => {
+        release();
+      });
+      logger.debug(`Flushing ${this.flushableRecordCounter} items from cache`);
+      const pendingFlush = Promise.all([
+        this.model.bulkCreate(Object.values(this.setCache), {transaction: tx, updateOnDuplicate: ['mmrRoot']}),
+        this.model.destroy({where: {id: this.removeCache}, transaction: tx}),
+      ]);
 
-    // Don't await DB operations to complete before clearing.
-    // This allows new data to be cached while flushing
-    this.clear();
+      // Don't await DB operations to complete before clearing.
+      // This allows new data to be cached while flushing
+      this.clear();
 
-    await pendingFlush;
+      await pendingFlush;
+    } catch (e) {
+      release();
+      throw e;
+    }
   }
 
   private mergeResultsWithCache(results: (ProofOfIndex | undefined)[], order: 'asc' | 'desc' = 'asc'): ProofOfIndex[] {
