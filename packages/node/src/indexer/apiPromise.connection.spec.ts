bb49e2bd0852a8e0197e7f3569296c143e1ebd6a
Naveen V <velnaveen99@gmail.com>
2023-05-30 11:51:58 +0800

    add a cache to WsProvider (#1760)
    * add a cache to WsProvider
    
    * extend cached provider to http and use LRUCache
    
    * add tests
    
    * test for cached ws provider
    
    * fix tests
    
    * code cleanup
    
    * code cleanup

---
+++ packages/node/src/indexer/apiPromise.connection.spec.ts
@@ -0,0 +1,64 @@
+// Copyright 2020-2022 OnFinality Limited authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { WsProvider } from '@polkadot/rpc-provider';
+import { createCachedProvider } from './x-provider/cachedProvider';
+import { HttpProvider } from './x-provider/http';
+
+const TEST_BLOCKHASH =
+  '0x3b712c10ba98c5421a468d1411c94479381be307b7ecb5b2602cf0d395eb4292';
+
+describe('ApiPromiseConnection', () => {
+  let wsProvider: WsProvider;
+  let httpProvider: HttpProvider;
+
+  beforeEach(async () => {
+    wsProvider = await new WsProvider(
+      'wss://kusama.api.onfinality.io/public-ws',
+    ).isReady;
+    httpProvider = new HttpProvider('https://kusama.api.onfinality.io/public');
+
+    jest.spyOn(wsProvider, 'send');
+    jest.spyOn(httpProvider, 'send');
+  });
+
+  it('should not make duplicate requests for state_getRuntimeVersion on wsProvider', async () => {
+    const cachedProvider = createCachedProvider(wsProvider);
+
+    await Promise.all([
+      cachedProvider.send('state_getRuntimeVersion', [TEST_BLOCKHASH]),
+      cachedProvider.send('state_getRuntimeVersion', [TEST_BLOCKHASH]),
+    ]);
+
+    expect(wsProvider.send).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not make duplicate requests for chain_getHeader on wsProvider', async () => {
+    const cachedProvider = createCachedProvider(wsProvider);
+    await Promise.all([
+      cachedProvider.send('chain_getHeader', [TEST_BLOCKHASH]),
+      cachedProvider.send('chain_getHeader', [TEST_BLOCKHASH]),
+    ]);
+    expect(wsProvider.send).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not make duplicate requests for state_getRuntimeVersion on httpProvider', async () => {
+    const cachedProvider = createCachedProvider(httpProvider);
+
+    await Promise.all([
+      cachedProvider.send('state_getRuntimeVersion', [TEST_BLOCKHASH]),
+      cachedProvider.send('state_getRuntimeVersion', [TEST_BLOCKHASH]),
+    ]);
+    expect(httpProvider.send).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not make duplicate requests for chain_getHeader on httpProvider', async () => {
+    const cachedProvider = createCachedProvider(httpProvider);
+
+    await Promise.all([
+      cachedProvider.send('chain_getHeader', [TEST_BLOCKHASH]),
+      cachedProvider.send('chain_getHeader', [TEST_BLOCKHASH]),
+    ]);
+    expect(httpProvider.send).toHaveBeenCalledTimes(1);
+  });
+});
