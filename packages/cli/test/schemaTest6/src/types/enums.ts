72f1cdf6b188d447fd4386d7d9d7bd71251573e8
bz888 <ziyu.ben158@gmail.com>
2023-06-01 14:04:55 +1200

    removed genrated src from ttest

--- packages/cli/test/schemaTest6/src/types/enums.ts
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-
-// Auto-generated , DO NOT EDIT
-
-export enum Test {
-  FOO = 'FOO',
-
-  BAR = 'BAR',
-}
--- packages/cli/test/schemaTest6/src/types/index.ts
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-
-// Auto-generated , DO NOT EDIT
-export * from './models';
-
-export * from './enums';
