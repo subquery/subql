6d11e60f244ea689d8a61cf7f1746edfb19b98a3
JQQQ <jiqiang90@hotmail.com>
2023-06-19 14:13:13 +1200

    fix test 20230619

--- packages/common/src/project/readers/ipfs-reader.spec.ts
+++ packages/common/src/project/readers/ipfs-reader.spec.ts
@@ -4,16 +4,16 @@
 import {IPFSReader} from './ipfs-reader';
 import {Reader} from './reader';
 
-const IPFSGateway = 'https://interipfs.thechaindata.com/ipfs/api/v0';
+const IPFSGateway = 'https://unauthipfs.subquery.network/ipfs/api/v0';
 
 describe('IPFSReader', () => {
   let reader: Reader;
 
   it('should return a project deployment', async () => {
-    reader = new IPFSReader('Qmdwxos2aDv8KebrLcs2Y9uANJ5SFawagDwKgiogRrrUjM', IPFSGateway);
+    reader = new IPFSReader('QmNbkA1fJpV2gCAWCBjgUQ8xBTwkLZHuzx4EkUoKx7VYaD', IPFSGateway);
 
     const data: any = await reader.getProjectSchema();
 
-    expect(data.network.genesisHash).toBe('0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3');
+    expect(data.network.chainId).toBe('43114');
   });
 });
