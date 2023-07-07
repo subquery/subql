f75f77757f4212778a597ecdd76561f943031237
bz888 <ziyu.ben158@gmail.com>
2023-07-07 15:38:13 +1200

    update ejs for scaffold

--- packages/cli/src/controller/scaffoldgen-controller.ts
+++ packages/cli/src/controller/scaffoldgen-controller.ts
@@ -19,11 +19,36 @@ export async function renderTemplate(templatePath: string, outputPath: string, t
 // after selecting a type, this should generate a handler in accordance
 export async function generateScaffold(handlerName: string, argType: any, projectPath: string): Promise<void> {
   // input type
+  const entities = [
+    {
+      name: 'Approval',
+      constructors: [
+        // Will loop through the entity and all the required fields
+        {id: 'hash'},
+        {owner: 'from'},
+        {spender: 'await tx.args[0]'}, // if Promise then should add await
+        {value: 'BigInt(await tx.args[1].toString())'}, // Should ensure types are correct
+        {contractAddress: 'to'},
+      ],
+    },
+    {
+      name: 'Transaction',
+      constructors: [],
+    },
+  ];
+
   await renderTemplate(SCAFFOLD_HANDLER_TEMPLATE_PATH, path.join(projectPath, ROOT_MAPPING_DIR, handlerName), {
     props: {
-      abiName: 'Erc20Abi',
-      argType: 'TransferLog',
+      abis: ['Erc20Abi'],
+      args: ['log', 'tx'],
+      argType: [
+        // these are from ABI generated
+        'TransferLog',
+        'ApproveTransaction',
+      ],
       handlerName: 'handleLog',
+      entities: [],
+      // how would i know what entity to generate
     },
     helper: {},
   });
