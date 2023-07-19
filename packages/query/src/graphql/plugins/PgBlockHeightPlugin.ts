511e0657900505e91df2dbc551217b565e46c47c
JQQQ <jiqiang90@hotmail.com>
2023-07-19 12:58:02 +1200

    fix missing update header

--- packages/query/src/graphql/plugins/PgBlockHeightPlugin.ts
+++ packages/query/src/graphql/plugins/PgBlockHeightPlugin.ts
@@ -1,5 +1,5 @@
-// Copyright 2022 OnFinality Limited authors & contributors
-// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020-2023 SubQuery Pte Ltd authors & contributors
+// SPDX-License-Identifier: GPL-3.0
 
 import {QueryBuilder} from '@subql/x-graphile-build-pg';
 import {Plugin} from 'graphile-build';
