17daff59ae3845acbd026cc49d4ead22a60c9d75
Naveen V <velnaveen99@gmail.com>
2023-07-26 03:46:24 +0000

    update `decodeScVal` with more ScValType

--- packages/node/src/soroban/block.soroban.ts
+++ packages/node/src/soroban/block.soroban.ts
@@ -57,24 +57,43 @@ export class SorobanBlockWrapped implements SorobanBlockWrapper {
     return true;
   }
 
-  static decodeScVals(value: xdr.ScVal): any {
-    switch (value.switch()) {
-      case xdr.ScValType.scvBytes(): {
-        const buffer = value.bytes();
-        return buffer.toString();
-      }
-      case xdr.ScValType.scvSymbol(): {
-        return value.sym().toString();
-      }
+  static decodeScVals(scVal: xdr.ScVal): any {
+    switch (scVal.switch()) {
+      case xdr.ScValType.scvBool():
+        return scVal.b();
+      case xdr.ScValType.scvSymbol():
+        return scVal.sym().toString();
+      case xdr.ScValType.scvU64():
+        return scVal.u64().low;
       case xdr.ScValType.scvAddress(): {
-        return Address.fromScVal(value).toString();
+        try {
+          return Address.account(
+            scVal.address().accountId().value(),
+          ).toString();
+        } catch (error) {
+          return Address.contract(scVal.address().contractId()).toString();
+        }
       }
       case xdr.ScValType.scvString(): {
-        return Buffer.from(value.str().toString(), 'base64').toString();
+        return Buffer.from(scVal.str().toString(), 'base64').toString();
       }
+      case xdr.ScValType.scvBytes():
+        return scVal.bytes();
+      case xdr.ScValType.scvI128(): {
+        const low = scVal.i128().lo();
+        const high = scVal.i128().hi();
+        return BigInt(low.low) | (BigInt(low.high) << BigInt(32));
+      }
+      case xdr.ScValType.scvMap():
+        return Object.fromEntries(
+          scVal.map()!.map((entry) => {
+            const key = entry.key().sym();
+            return [key, SorobanBlockWrapped.decodeScVals(entry.val())];
+          }),
+        );
       default:
         throw new Error(
-          `Unable to decode event: Unknown ScValType ${value.switch().name}`,
+          `Unable to decode event: Unknown ScValType ${scVal.switch().name}`,
         );
     }
   }
