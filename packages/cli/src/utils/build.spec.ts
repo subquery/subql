af4860a7a2744e2e91a65fb468bf1641356d67c2
Naveen V <velnaveen99@gmail.com>
2023-10-16 15:33:16 +0000

    add unit tests

--- packages/cli/src/utils/build.spec.ts
+++ packages/cli/src/utils/build.spec.ts
@@ -7,11 +7,16 @@ import {Command} from '@oclif/core';
 import {MultichainProjectManifest} from '@subql/types-core';
 import * as yaml from 'js-yaml';
 import rimraf from 'rimraf';
-import {buildManifestFromLocation, generateManifestFromTs} from './build';
+import {buildManifestFromLocation} from './build';
 
 describe('Manifest generation', () => {
   afterEach(() => {
-    jest.clearAllMocks();
+    const projectPath = path.join(__dirname, '../../test/tsManifestTest');
+    rimraf.sync(path.join(projectPath, 'project1.yaml'));
+    rimraf.sync(path.join(projectPath, 'subquery-multichain2.yaml'));
+    rimraf.sync(path.join(projectPath, 'project2.yaml'));
+    rimraf.sync(path.join(projectPath, 'subquery-multichain.yaml'));
+    rimraf.sync(path.join(projectPath, 'subquery-multichain3.yaml'));
   });
 
   it('should build ts manifest from multichain file', async () => {
@@ -28,9 +33,41 @@ describe('Manifest generation', () => {
       readFileSync(path.join(projectPath, '../subquery-multichain.yaml'), 'utf8')
     ) as MultichainProjectManifest;
     multichainContent.projects.forEach((project) => project.endsWith('.yaml'));
+  }, 50000);
 
-    rimraf.sync(path.join(projectPath, '../project1.yaml'));
-    rimraf.sync(path.join(projectPath, '../project2.yaml'));
-    rimraf.sync(path.join(projectPath, '../subquery-multichain.yaml'));
+  it('throws error on unknown file in multichain manifest', async () => {
+    const projectPath = path.join(__dirname, '../../test/tsManifestTest/subquery-multichain2.ts');
+    await expect(buildManifestFromLocation(projectPath, {log: console.log} as unknown as Command)).rejects.toThrow();
+  }, 50000);
+
+  it('allows both ts and yaml file in multichain manifest', async () => {
+    const projectPath = path.join(__dirname, '../../test/tsManifestTest/subquery-multichain3.ts');
+    await expect(
+      buildManifestFromLocation(projectPath, {log: console.log} as unknown as Command)
+    ).resolves.toBeDefined();
+    expect(existsSync(path.join(projectPath, '../project1.yaml'))).toBe(true);
+    expect(existsSync(path.join(projectPath, '../project3.yaml'))).toBe(true);
+    expect(existsSync(path.join(projectPath, '../subquery-multichain3.yaml'))).toBe(true);
+
+    //ts files are replaced with yaml files
+    const multichainContent = yaml.load(
+      readFileSync(path.join(projectPath, '../subquery-multichain3.yaml'), 'utf8')
+    ) as MultichainProjectManifest;
+    multichainContent.projects.forEach((project) => project.endsWith('.yaml'));
+  }, 50000);
+
+  it('should build ts manifest from yaml multichain file', async () => {
+    const projectPath = path.join(__dirname, '../../test/tsManifestTest/subquery-multichain4.yaml');
+    await expect(
+      buildManifestFromLocation(projectPath, {log: console.log} as unknown as Command)
+    ).resolves.toBeDefined();
+    expect(existsSync(path.join(projectPath, '../project1.yaml'))).toBe(true);
+    expect(existsSync(path.join(projectPath, '../project2.yaml'))).toBe(true);
+
+    //ts files are replaced with yaml files
+    const multichainContent = yaml.load(
+      readFileSync(path.join(projectPath, '../subquery-multichain4.yaml'), 'utf8')
+    ) as MultichainProjectManifest;
+    multichainContent.projects.forEach((project) => project.endsWith('.yaml'));
   }, 50000);
 });
