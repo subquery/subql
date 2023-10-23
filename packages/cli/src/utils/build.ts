045a431272cae0f6b36b5ffbb399f28df523c74f
Scott Twiname <skott.twiname@gmail.com>
2023-10-24 10:54:17 +1300

    Fix ts manifest build failing because of missing deps

--- packages/cli/src/utils/build.ts
+++ packages/cli/src/utils/build.ts
@@ -1,10 +1,8 @@
 // Copyright 2020-2023 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: GPL-3.0
 
-import {execFile} from 'child_process';
 import {assert} from 'console';
 import {existsSync, lstatSync, readFileSync, writeFileSync} from 'fs';
-import util from 'node:util';
 import path from 'path';
 import {Command} from '@oclif/core';
 import {
@@ -15,6 +13,7 @@ import {
 } from '@subql/common';
 import {MultichainProjectManifest} from '@subql/types-core';
 import * as yaml from 'js-yaml';
+import ts from 'typescript';
 
 const requireScriptWrapper = (scriptPath: string, outputPath: string): string =>
   `import {toJsonObject} from '@subql/common';` +
@@ -51,20 +50,23 @@ export async function buildManifestFromLocation(location: string, command: Comma
       replaceTsReferencesInMultichain(projectYamlPath);
     }
   } catch (e) {
+    console.log(e)
     throw new Error(`Failed to generate manifest from typescript ${projectManifestEntry}, ${e.message}`);
   }
   return directory;
 }
-
+// eslint-disable-next-line @typescript-eslint/require-await
 export async function generateManifestFromTs(projectManifestEntry: string, command: Command): Promise<string> {
   assert(existsSync(projectManifestEntry), `${projectManifestEntry} does not exist`);
   const projectYamlPath = tsProjectYamlPath(projectManifestEntry);
   try {
-    await util.promisify(execFile)(
-      'npx',
-      ['ts-node', '-e', requireScriptWrapper(projectManifestEntry, projectYamlPath)],
-      {cwd: path.dirname(projectManifestEntry)}
-    );
+    const result = ts.transpileModule(requireScriptWrapper(projectManifestEntry, projectYamlPath), {});
+
+    console.log('OUTPUT', result)
+
+    // Run the compiled output
+    eval(result.outputText);
+
     command.log(`Project manifest generated to ${projectYamlPath}`);
 
     return projectYamlPath;
