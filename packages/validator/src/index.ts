924aed299dc4ad0745d945dec827f214719ec479
Jay Ji <jiqiang90@hotmail.com>
2023-10-25 08:54:26 +1300

    remove @subql/validator (#2121)
    * remove @subql/validator
    
    * changelog

--- packages/validator/src/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2020-2023 SubQuery Pte Ltd authors & contributors
-// SPDX-License-Identifier: GPL-3.0
-
-export * from './validator';
-export * from './rules';
--- packages/validator/src/rules/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright 2020-2023 SubQuery Pte Ltd authors & contributors
-// SPDX-License-Identifier: GPL-3.0
-
-export * from './rule';
-export * from './require-build-script';
-export * from './require-codegen-script';
-export * from './require-cli-dep';
-export * from './require-valid-manifest';
-export * from './require-valid-runner';
