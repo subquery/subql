0ef8cdfabd18b6318b8149dd0aa8a501449fa6f4
Naveen V <velnaveen99@gmail.com>
2023-10-27 08:24:03 +0000

    debug replace-versions.sh

--- .github/workflows/scripts/replace-versions.sh
+++ .github/workflows/scripts/replace-versions.sh
@@ -10,15 +10,25 @@ packages["./packages/types"]="@subql/types"
 packages["./packages/utils"]="@subql/utils"
 packages["./packages/testing"]="@subql/testing"
 
+# Path to the node package.json
+node_package_json="./packages/node/package.json"
+
 # Iterate over each package
 for package in ${!packages[@]}
 do
   # Get the version of the current package
   version=$(jq -r '.version' $package/package.json)
   echo $package
-  echo $version  
-  # Replace workspace:* with the actual version in package.json of node
-  sed -i.bak "s#\"${packages[$package]}\": \"workspace:\*\"#\"${packages[$package]}\": \"$version\"#g" ./packages/node/package.json
-  # Remove the backup file
-  rm ./packages/node/package.json.bak
+  echo $version
+
+  # Check if the node package.json file exists
+  if [[ -f $node_package_json ]]; then
+    # Replace workspace:* with the actual version in package.json of node
+    sed -i.bak "s#\"${packages[$package]}\": \"workspace:\*\"#\"${packages[$package]}\": \"$version\"#g" $node_package_json
+    # Remove the backup file
+    rm "${node_package_json}.bak"
+  else
+    echo "Error: $node_package_json does not exist."
+    exit 1
+  fi
 done
