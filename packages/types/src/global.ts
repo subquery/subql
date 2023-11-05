e824591fc9af7fc0391335092d525d38033191b3
Ben <89335033+bz888@users.noreply.github.com>
2023-11-06 10:51:28 +1300

    add missing global registry var (#199)
    * add missing global registry var
    
    * restore types-core global var

--- packages/types/src/global.ts
+++ packages/types/src/global.ts
@@ -2,10 +2,12 @@
 // SPDX-License-Identifier: GPL-3.0
 
 import type {CosmWasmClient} from '@cosmjs/cosmwasm-stargate';
+import {Registry} from '@cosmjs/proto-signing';
 import {CosmWasmSafeClient} from './interfaces';
 import '@subql/types-core/dist/global';
 
 declare global {
   const apiUnsafe: CosmWasmClient; //requires --unsafe flag to be defined
   const api: CosmWasmSafeClient;
+  const registry: Registry;
 }
