ffb1ce9cc7b23ade948aa1b197904f9632ccb0cc
Scott Twiname <skott.twiname@gmail.com>
2023-11-07 10:10:19 +1300

    Add comment

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@ apk add --no-cache jq
 npm install -g --force yarn@latest
 cd "$1"
 
+# Modifies the package.json to replace "workspace:*" versions with actual versions
 jq -r '.dependencies | to_entries[] | select(.value == "workspace:*") | .key' package.json | while read -r dep; do
   directory=$(jq --arg dep "$dep" -r '.compilerOptions.paths[$dep][0]' ../../tsconfig.json | cut -d'/' -f 2)
   version=$(jq --arg directory "$directory" -r '.version' ../"$directory"/package.json)
