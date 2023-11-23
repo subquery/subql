1be21f15bf522fefe74fc3f463cffe53ac46fe80
bz888 <ziyu.ben158@gmail.com>
2023-11-24 10:25:21 +1300

    clean code

---
+++ packages/node-core/src/configure/SchemaMigration.service.spec.ts
@@ -0,0 +1,11 @@
+// Copyright 2020-2023 SubQuery Pte Ltd authors & contributors
+// SPDX-License-Identifier: GPL-3.0
+
+describe('SchemaMigration', () => {
+  describe('comparator', () => {
+    // add new entity
+    // remove entity
+    // modify entity fields
+    // derived from
+  });
+});
