75f5bf76653022541b69824fa102dc6795b6a883
bz888 <ziyu.ben158@gmail.com>
2024-01-25 11:04:19 +1300

    update docker-compose on root

--- packages/node/src/configure/SchemaMigration.service.test.ts
+++ packages/node/src/configure/SchemaMigration.service.test.ts
@@ -448,7 +448,7 @@ describe('SchemaMigration integration tests', () => {
       ),
     ).toBe(false);
   });
-  it.skip('On Failed migration, no metadata transaction should be applied', async () => {
+  it('On Failed migration, no metadata transaction should be applied', async () => {
     const processExitSpy = jest
       .spyOn(process, 'exit')
       .mockImplementationOnce((() => {
