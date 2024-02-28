a7e63b59d6d30304dc2bd1abdcd450d957c829cf
JQQQ <jiqiang90@hotmail.com>
2024-02-29 12:02:59 +1300

    update

--- packages/utils/src/types/generalTypes.spec.ts
+++ packages/utils/src/types/generalTypes.spec.ts
@@ -1,9 +1,9 @@
 // Copyright 2020-2024 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: GPL-3.0
 
-import {numberToU8a} from '@polkadot/util';
+import {isU8a} from '@polkadot/util';
 import {getTypeByScalarName} from '../types';
-import {isNegativeU8a, negativeToU8a, negativeU8aToNum} from './u8aUtils';
+import {negativeToU8a} from './u8aUtils';
 
 describe('general types', () => {
   it('can get json type', () => {
@@ -20,32 +20,17 @@ describe('general types', () => {
     expect(getTypeByScalarName('Unsupported')).toBe(undefined);
   });
 
-  it('Int type should able to hash negative value, also revertible', () => {
+  it('Int type should able to hash negative value', () => {
     const testNum = -1;
-    const testNum2 = -9898999;
-
+    const testNum2 = -989899999;
     const testNum3 = 10000;
-
+    const testNum4 = -98989999999999;
     const testArray = negativeToU8a(testNum);
     const testArray2 = negativeToU8a(testNum2);
-
+    const testArray4 = negativeToU8a(testNum4);
     expect(() => negativeToU8a(testNum3)).toThrow();
-
-    const testArrayIsNegative = isNegativeU8a(testArray);
-    const testArray2IsNegative = isNegativeU8a(testArray2);
-
-    // a positive number U8a should be detected
-    const polkadotU8a = numberToU8a(testNum3);
-    const testArray3IsNegative = isNegativeU8a(polkadotU8a);
-
-    expect(testArrayIsNegative).toBeTruthy();
-    expect(testArray2IsNegative).toBeTruthy();
-    expect(testArray3IsNegative).toBeFalsy();
-
-    const revertTestNum = negativeU8aToNum(testArray);
-    const revertTestNum2 = negativeU8aToNum(testArray2);
-
-    expect(testNum).toBe(revertTestNum);
-    expect(testNum2).toBe(revertTestNum2);
+    expect(isU8a(testArray)).toBeTruthy();
+    expect(isU8a(testArray2)).toBeTruthy();
+    expect(isU8a(testArray4)).toBeTruthy();
   });
 });
--- packages/utils/src/types/u8aUtils.ts
+++ packages/utils/src/types/u8aUtils.ts
@@ -1,42 +1,19 @@
 // Copyright 2020-2024 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: GPL-3.0
 
+import {hexToU8a, numberToHex} from '@polkadot/util';
+
 /**
  * Due to current polkadotjs `numberToU8a` not support negative number,
- * These helper method used to convert negative to uint8Array, and revert it to number
+ * The helper method used to convert negative to a uint8Array
  *
  * @param byteArray
  */
-
-export function isNegativeU8a(byteArray: Uint8Array): boolean {
-  return (byteArray[0] & 0x80) !== 0;
-}
-
-export function negativeU8aToNum(byteArray: Uint8Array) {
-  if (!isNegativeU8a(byteArray)) {
-    throw new Error(`byteArray didn't represent a negative number`);
-  }
-  const hexString = Array.from(byteArray, function (byte) {
-    return `0${(byte & 0xff).toString(16)}`.slice(-2);
-  }).join('');
-
-  let num = parseInt(hexString, 16);
-  if (num > 2147483647) {
-    num -= 4294967296; // Subtract 2^32 for negative numbers
-  }
-  return num;
-}
 
 export function negativeToU8a(num: number) {
   if (num > 0) {
     throw new Error(`number must be negative`);
   }
-  const hexString = (num >>> 0).toString(16);
-  const byteArray = new Uint8Array(hexString.length / 2); // Create Uint8Array
-  // Fill Uint8Array with bytes from hexadecimal representation
-  for (let i = 0; i < hexString.length; i += 2) {
-    const byte = parseInt(hexString.substr(i, 2), 16);
-    byteArray[i / 2] = byte;
-  }
-  return byteArray;
+  const hexString = `0${numberToHex(Math.abs(num))}`;
+  return hexToU8a(hexString);
 }
