6a5ae432bafcf55b2dd927a3cbe423b88568129b
Jay Ji <jiqiang90@hotmail.com>
2024-02-29 13:26:51 +1300

    Update packages/utils/src/types/u8aUtils.ts
    Co-authored-by: Scott Twiname <skott.twiname@gmail.com>

--- packages/utils/src/types/u8aUtils.ts
+++ packages/utils/src/types/u8aUtils.ts
@@ -11,7 +11,7 @@ import {numberToU8a, u8aConcat} from '@polkadot/util';
  */
 
 export function wrappedNumToU8a(num: number) {
-  if (num > 0) {
+  if (num >= 0) {
     return numberToU8a(num);
   }
   return u8aConcat(new Uint8Array([0]), numberToU8a(Math.abs(num)));
