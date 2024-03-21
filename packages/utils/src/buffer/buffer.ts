b28d8822386b7b46ab874e15eb3679ad938f0ae8
Jay Ji <jiqiang90@hotmail.com>
2024-03-21 15:36:08 +1300

    Fix util number to hex (#2319)
    * Fix util number to hex
    
    * update for 0

--- packages/utils/src/buffer/buffer.ts
+++ packages/utils/src/buffer/buffer.ts
@@ -1,6 +1,9 @@
 // Copyright 2020-2024 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: GPL-3.0
 
+import {isHex} from '@polkadot/util';
+import {HexString} from '@polkadot/util/types';
+
 export {
   u8aEq,
   hexToU8a,
@@ -18,3 +21,20 @@ export {
 } from '@polkadot/util';
 
 export {blake2AsHex, blake2AsU8a, isBase58, isBase64, base58Decode, base64Decode} from '@polkadot/util-crypto';
+
+// This is a clone from ethers.util hexStripZeros, in order to match polkadot util hex value with ethers
+export function hexStripZeros(value: HexString): string {
+  if (!isHex(value)) {
+    throw new Error(`invalid hex string ${value}`);
+  }
+  const str = value.toString().substring(2);
+  let offset = 0;
+  while (offset < str.length && str[offset] === '0') {
+    offset++;
+  }
+  const trimmed = `0x${str.substring(offset)}`;
+  if (trimmed === '0x') {
+    return '0x0';
+  }
+  return trimmed;
+}
