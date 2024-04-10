1dc7c791808de2e90feceea5a7df67ff8cdf251a
Jay Ji <jiqiang90@hotmail.com>
2024-04-10 12:06:39 +1200

    Update header to 2024 (#56)

--- packages/node/src/stellar/soroban.server.spec.ts
+++ packages/node/src/stellar/soroban.server.spec.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2023 SubQuery Pte Ltd authors & contributors
+// Copyright 2020-2024 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: GPL-3.0
 
 import { SorobanRpc } from 'stellar-sdk';
