7e776a98d37ab0b103e5ab93c03ccdbe3eb1327b
JQQQ <jiqiang90@hotmail.com>
2024-04-30 15:17:12 +1200

    update for mapping

--- packages/cli/src/controller/migrate/mapping/migrate-mapping.controller.ts
+++ packages/cli/src/controller/migrate/mapping/migrate-mapping.controller.ts
@@ -4,14 +4,12 @@
 import fs from 'fs';
 import path from 'path';
 import {copyFolderSync} from '../../../utils';
-import {ROOT_MAPPING_DIR} from '../../generate-controller';
 
-export async function migrateMapping(subgraphDir: string, subqlDir: string) {
-  const subqlMappingPath = path.join(subqlDir, ROOT_MAPPING_DIR);
-  await fs.promises.rm(subqlMappingPath, {force: true, recursive: true});
-  await fs.promises.mkdir(subqlMappingPath, {recursive: true});
+export async function migrateMapping(subgraphDir: string, subqlDir: string): Promise<void> {
+  const subqlSrcPath = path.join(subqlDir, '/src');
+  await fs.promises.rm(subqlSrcPath, {force: true, recursive: true});
   // copy over src
-  copyFolderSync(path.join(subgraphDir, '/src'), path.join(subqlDir, '/src'));
+  copyFolderSync(path.join(subgraphDir, '/src'), subqlSrcPath);
   console.log(
     `* Mapping handlers have been copied over, they will need to be updated to work with SubQuery. See our documentation for more details https://academy.subquery.network/build/graph-migration.html#codegen`
   );
