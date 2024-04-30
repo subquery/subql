d78661747a3b1464523545cb5f19b31c199ee237
JQQQ <jiqiang90@hotmail.com>
2024-04-30 17:02:54 +1200

    add more evm network to chainID

--- packages/cli/src/controller/migrate/constants.ts
+++ packages/cli/src/controller/migrate/constants.ts
@@ -34,5 +34,31 @@ export const subqlNetworkTemplateNetwork: Record<NETWORK_FAMILY, NetworkExampleP
 };
 
 export const graphNetworkNameChainId: Partial<Record<NETWORK_FAMILY, Record<string, string>>> = {
-  [NETWORK_FAMILY.ethereum]: {mainnet: '1'},
+  [NETWORK_FAMILY.ethereum]: {
+    mainnet: '1',
+    sepolia: '11155111',
+    optimism: '10',
+    aurora: '1313161554',
+    bsc: '56',
+    gnosis: '100',
+    fuse: '122',
+    moonbeam: '1284',
+    moonriver: '1285',
+    mbase: '1287',
+    clover: '1023',
+    scroll: '534352',
+    matic: '137',
+    mumbai: '80001',
+    avalanche: '43114',
+    fuji: '43113',
+    'arbitrum-sepolia': '421614',
+    'fantom-testnet': '4002',
+    'optimism-sepolia': '11155420',
+    'blast-mainnet': '81457',
+    'blast-testnet': '168587773',
+    'arbitrum-one': '42161',
+    'astar-zkevm-mainnet': '3776',
+    'base-sepolia': '84532',
+    'polygon-zkevm': '84532',
+  },
 };
