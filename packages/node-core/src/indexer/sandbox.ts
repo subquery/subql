ff50acd164750de6c8300d44ff3ddba56d168ed1
JQQQ <jiqiang90@hotmail.com>
2024-05-01 22:01:19 +1200

    Fix sandbox buffer uint8Array concat issue

--- packages/node-core/src/indexer/sandbox.ts
+++ packages/node-core/src/indexer/sandbox.ts
@@ -25,7 +25,7 @@ const DEFAULT_OPTION = (unsafe = false): NodeVMOptions => {
   return {
     console: 'redirect',
     wasm: unsafe,
-    sandbox: {},
+    sandbox: {Uint8Array},
     require: {
       builtin: unsafe ? ['*'] : ['assert', 'buffer', 'crypto', 'util', 'path', 'url'],
       external: true,
---
+++ packages/node-core/test/sandbox/buffer-test.ts
@@ -0,0 +1,31 @@
+// Copyright 2020-2024 SubQuery Pte Ltd authors & contributors
+// SPDX-License-Identifier: GPL-3.0
+
+export async function testSandbox(): Promise<void> {
+  const str = 'Hello, Buffer!';
+  const strBuffer = Buffer.from(str);
+  const uin8Array = new Uint8Array(2);
+  uin8Array[0] = 72; //'H'
+  uin8Array[1] = 101; //'e'
+  try {
+    const output = Buffer.concat([strBuffer, uin8Array]);
+    console.log(output.toString()); //expect to be `Hello, Buffer!He`
+  } catch (e) {
+    throw new Error(`Buffer concat failed, ${e}`);
+  }
+}
+
+export async function testUin8Array(): Promise<void> {
+  const uin8Array = new Uint8Array(2);
+  const uin8Array2 = new Uint8Array(2);
+  uin8Array[0] = 72; //'H'
+  uin8Array[1] = 101; //'e'
+  uin8Array2[0] = 72;
+  uin8Array2[1] = 101;
+  try {
+    const output = Buffer.concat([uin8Array, uin8Array2]);
+    console.log(output.toString()); //expect to be `HeHe`
+  } catch (e) {
+    throw new Error(`Uin8Array concat failed, ${e}`);
+  }
+}
