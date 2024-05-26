a0768455852cfbce225ff353f6711ce398bc8e00
Scott Twiname <skott.twiname@gmail.com>
2024-05-27 11:11:19 +1200

    Add tests and changelog for previous PR (#2412)

--- packages/cli/src/controller/codegen-controller.test.ts
+++ packages/cli/src/controller/codegen-controller.test.ts
@@ -24,12 +24,14 @@ describe('Codegen can generate schema', () => {
     const projectPath = path.join(__dirname, '../../test/schemaTest');
     await expect(codegen(projectPath, ['project-bad-schema.yaml'])).rejects.toThrow(/is not an valid type/);
   });
+
   it('codegen with entities that uses reserved names should throw', async () => {
     const projectPath = path.join(__dirname, '../../test/schemaTest');
     await expect(codegen(projectPath, ['project-bad-entity.yaml'])).rejects.toThrow(
       'EntityName: exampleEntityFilter cannot end with reservedKey: filter'
     );
   });
+
   it('Codegen should be able to generate ABIs from template datasources', async () => {
     const projectPath = path.join(__dirname, '../../test/schemaTest');
     await codegen(projectPath, ['project-templates-abi.yaml']);
@@ -42,6 +44,7 @@ describe('Codegen can generate schema', () => {
     const projectPath = path.join(__dirname, '../../test/schemaTest');
     await expect(codegen(projectPath, ['project-no-assets.yaml'])).resolves.not.toThrow();
   });
+
   it('Codegen should be able to generate ABIs from customName datasources', async () => {
     const projectPath = path.join(__dirname, '../../test/schemaTest');
     await codegen(projectPath);
@@ -58,6 +61,7 @@ describe('Codegen can generate schema', () => {
     // should not contain abi directory
     await expect(fs.promises.readFile(`${projectPath}/src/types/abi-interfaces/Erc721.ts`, 'utf8')).rejects.toThrow();
   });
+
   it('should generate contracts on different glob paths', async () => {
     const projectPath = path.join(__dirname, '../../test/schemaTest');
     await codegen(projectPath, ['typechain-test.yaml']);
@@ -74,14 +78,29 @@ describe('Codegen can generate schema', () => {
       fs.promises.readFile(`${projectPath}/src/types/abi-interfaces/Artifact.ts`, 'utf8')
     ).resolves.toBeTruthy();
   });
+
   it('Should not generate ABI for non evm ds', async () => {
     const projectPath = path.join(__dirname, '../../test/schemaTest');
     await codegen(projectPath, ['non-evm-project.yaml']);
     expect(fs.existsSync(`${projectPath}/src/types/abi-interfaces/`)).toBeFalsy();
   });
+
   it('Should not generate proto-interfaces if no chaintypes are provided', async () => {
     const projectPath = path.join(__dirname, '../../test/schemaTest');
     await codegen(projectPath, ['project-cosmos.yaml']);
     expect(fs.existsSync(`${projectPath}/src/types/proto-interfaces/`)).toBeFalsy();
+  });
+
+  it('Should dedupe enums', async () => {
+    const projectPath = path.join(__dirname, '../../test/schemaTest');
+    await codegen(projectPath, ['project-duplicate-enum.yaml']);
+
+    const fooFile = await fs.promises.readFile(`${projectPath}/src/types/models/foo.ts`, 'utf8');
+
+    expect(fooFile).toContain(
+      `import {
+    Bar,
+} from '../enums';`
+    );
   });
 });
