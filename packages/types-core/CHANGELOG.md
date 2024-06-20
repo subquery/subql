43a134c00ccb8b17d2996b17fcaad8f228facb93
JQQQ <jiqiang90@hotmail.com>
2024-06-20 15:37:12 +1200

    generic network module types

--- packages/types-core/CHANGELOG.md
+++ packages/types-core/CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+- Add generic network module types
+
 ### Changed
 - Reader interface not returning undefined
 
---
+++ packages/types-core/src/project/modulars/types.ts
@@ -0,0 +1,11 @@
+// Copyright 2020-2024 SubQuery Pte Ltd authors & contributors
+// SPDX-License-Identifier: GPL-3.0
+
+import {BaseDataSource, IProjectManifest} from '../index';
+
+export interface INetworkCommonModule<D extends BaseDataSource = BaseDataSource> {
+  parseProjectManifest(raw: unknown): IProjectManifest<D>;
+  isRuntimeDs(ds: D): boolean;
+  isCustomDs(ds: D): boolean;
+  validateProjectManifest(projectPath: string): Promise<boolean>;
+}
