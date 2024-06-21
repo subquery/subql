18598e884352c0be87ae1c7beba3f3c28f0eb2d6
JQQQ <jiqiang90@hotmail.com>
2024-06-21 16:39:10 +1200

    apply export type check

--- packages/types-core/src/project/modulars/types.ts
+++ packages/types-core/src/project/modulars/types.ts
@@ -3,8 +3,12 @@
 
 import {BaseCustomDataSource, BaseDataSource, IProjectManifest} from '../index';
 
-export interface INetworkCommonModule<D extends BaseCustomDataSource | BaseDataSource = BaseDataSource> {
+export interface INetworkCommonModule<
+  D extends BaseCustomDataSource | BaseDataSource = BaseDataSource,
+  RDS extends D = D,
+  CDS extends D = D,
+> {
   parseProjectManifest(raw: unknown): IProjectManifest<D>;
-  isRuntimeDs<DS extends D = D>(ds: D): ds is DS;
-  isCustomDs<CDS extends D = D>(ds: D): ds is CDS;
+  isRuntimeDs(ds: D): ds is RDS;
+  isCustomDs(ds: D): ds is CDS;
 }
