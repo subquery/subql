b8d20ff6097500705fc00e958dd6c9b671c86f92
Jay Ji <jiqiang90@hotmail.com>
2024-07-10 11:30:10 +1200

    Remove ipfs types, simplify code and get tests passing (#2483)
    Co-authored-by: Scott Twiname <skott.twiname@gmail.com>

--- packages/common/src/project/IpfsHttpClientLite/IPFSHTTPClientLite.ts
+++ packages/common/src/project/IpfsHttpClientLite/IPFSHTTPClientLite.ts
@@ -1,28 +1,40 @@
 // Copyright 2020-2024 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: GPL-3.0
 
-import {addAll} from '@subql/common/project/IpfsHttpClientLite/addAll';
 import axios from 'axios';
 import FormData from 'form-data';
-import type {Pin} from 'ipfs-core-types/types/src/pin/remote/index';
-import type {AddOptions, AddResult} from 'ipfs-core-types/types/src/root';
-import type {AbortOptions, ImportCandidate} from 'ipfs-core-types/types/src/utils';
-import type {HTTPClientExtraOptions} from 'ipfs-http-client/types/src/types';
-import {CID} from 'multiformats/cid';
-import {LiteAddAllOptions, IPFSOptions} from './interfaces';
-import {asyncIterableFromStream, decodePin} from './utils';
+import {asyncIterableFromStream} from './utils';
+
+type ContentData = string | Uint8Array;
+
+type Content =
+  | {
+      content: ContentData;
+      path: string;
+    }
+  | ContentData;
+
+type AddOptions = {
+  pin?: boolean;
+  cidVersion?: number;
+  wrapWithDirectory?: boolean;
+};
+
+type AddResult = {
+  path: string;
+  cid: string;
+  size: number;
+};
 
 export class IPFSHTTPClientLite {
-  private option: IPFSOptions;
-
-  constructor(option: IPFSOptions) {
+  constructor(private option: {url: string; headers?: Record<string, string>}) {
+    if (option.url === undefined) {
+      throw new Error('url is required');
+    }
     this.option = option;
   }
 
   get url(): string {
-    if (this.option.url === undefined) {
-      throw new Error('url is required');
-    }
     return this.option.url.toString();
   }
 
@@ -45,35 +57,22 @@ export class IPFSHTTPClientLite {
   /**
    * Import a file or data into IPFS
    */
-  async add(content: ImportCandidate, options?: AddOptions): Promise<AddResult> {
-    const addUrl = `${this.url}/add`;
-    const data = new FormData();
-    if (content instanceof Uint8Array) {
-      content = Buffer.from(content);
-    }
-    data.append('data', content);
-    const response = await axios.post(addUrl, data, {
-      headers: {
-        ...data.getHeaders(),
-        ...this.option.headers,
-      },
-      params: options,
-    });
+  async add(content: Content, options?: AddOptions): Promise<AddResult> {
+    const results = await this.addAll([content], options);
 
-    const {Hash, Path, Size} = response.data;
-    return {cid: CID.parse(Hash), size: Size, path: Path};
+    return results[0];
   }
 
   /**
    * Pin a content with a given CID to a remote pinning service.
    */
-  async pinRemoteAdd(cid: CID, options: {service: string}): Promise<Pin> {
-    const addUrl = `${this.url}/pin/remote/add`;
-    // For our own use, we only limited to these args
-    const urlWithParams = `${addUrl}?arg=${cid}&service=${options.service}`;
+  async pinRemoteAdd(cid: string, options: {service: string}): Promise<{Cid: string; Name: string; Status: string}> {
+    const url = new URL(`${this.url}/pin/remote/add`);
+    url.searchParams.append('arg', cid);
+    url.searchParams.append('service', options.service);
     try {
       const response = await axios.post(
-        urlWithParams,
+        url.toString(),
         {},
         {
           headers: {
@@ -81,7 +80,7 @@ export class IPFSHTTPClientLite {
           },
         }
       );
-      return decodePin(response.data);
+      return response.data;
     } catch (e) {
       throw new Error(`Failed to pin CID ${cid} to remote service`, {cause: e});
     }
@@ -90,12 +89,57 @@ export class IPFSHTTPClientLite {
   /**
    * Import multiple files and data into IPFS
    */
+  async addAll(source: Content[], options?: AddOptions): Promise<AddResult[]> {
+    const formData = this.makeFormData(source);
 
-  addAll(
-    source: ImportCandidate[],
-    options?: LiteAddAllOptions & AbortOptions & HTTPClientExtraOptions
-  ): AsyncIterable<AddResult> {
-    const addUrl = `${this.url}/add`;
-    return addAll(addUrl, source, {...options, headers: this.option.headers});
+    const url = new URL(`${this.url}/add`);
+    if (options) {
+      url.searchParams.append('pin', options.pin?.toString() ?? 'true');
+      url.searchParams.append('cid-version', options.cidVersion?.toString() ?? '0');
+      url.searchParams.append('wrap-with-directory', options.wrapWithDirectory?.toString() ?? 'false');
+    }
+
+    try {
+      const response = await axios.post(url.toString(), formData, {
+        headers: {
+          ...this.option.headers,
+          ...formData.getHeaders(),
+        },
+        maxBodyLength: Infinity,
+        maxContentLength: Infinity,
+      });
+
+      const mapResponse = (raw: any): AddResult => ({
+        path: raw.Name,
+        cid: raw.Hash,
+        size: parseInt(raw.Size, 10),
+      });
+
+      // If only one file is uploaded then the response is an object.
+      if (typeof response.data === 'object') {
+        return [mapResponse(response.data)];
+      }
+
+      const jsonLines = (response.data.split('\n') as string[]).filter((l) => l !== '');
+
+      return jsonLines.map((line) => JSON.parse(line)).map(mapResponse);
+    } catch (error) {
+      throw new Error(`Failed to upload files to IPFS`, {cause: error});
+    }
+  }
+
+  private makeFormData(contents: Content[]): FormData {
+    const formData = new FormData();
+    for (const content of contents) {
+      if (content instanceof Uint8Array) {
+        formData.append('data', Buffer.from(content));
+      } else if (typeof content === 'string') {
+        formData.append('data', content);
+      } else {
+        formData.append('data', content.content, {filename: content.path});
+      }
+    }
+
+    return formData;
   }
 }
--- packages/common/src/project/IpfsHttpClientLite/utils.ts
+++ packages/common/src/project/IpfsHttpClientLite/utils.ts
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
 import {AxiosResponse} from 'axios';
-import type {Pin, Status} from 'ipfs-core-types/types/src/pin/remote/index';
-import {CID} from 'multiformats/cid';
 
 export async function* asyncIterableFromStream(response: Promise<AxiosResponse>): AsyncIterable<Uint8Array> {
   const stream = (await response).data;
@@ -11,11 +9,3 @@ export async function* asyncIterableFromStream(response: Promise<AxiosResponse>)
     yield new Uint8Array(Buffer.from(chunk));
   }
 }
-
-export const decodePin = ({Cid: cid, Name: name, Status: status}: {Cid: string; Name: string; Status: Status}): Pin => {
-  return {
-    cid: CID.parse(cid),
-    name,
-    status,
-  };
-};
