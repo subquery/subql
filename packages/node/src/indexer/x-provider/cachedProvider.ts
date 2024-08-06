bb9c92e0db3bde62ef57a86db1c942989450576a
Scott Twiname <skott.twiname@gmail.com>
2024-08-06 14:24:51 +1200

    Fix up tests

--- packages/node/src/indexer/x-provider/cachedProvider.ts
+++ packages/node/src/indexer/x-provider/cachedProvider.ts
@@ -7,7 +7,6 @@ import { LRUCache } from 'lru-cache';
 const MAX_CACHE_SIZE = 200;
 const CACHE_TTL = 60 * 1000;
 
-/* eslint-disable prefer-rest-params */
 export function createCachedProvider(
   provider: ProviderInterface,
 ): ProviderInterface {
@@ -17,14 +16,13 @@ export function createCachedProvider(
   });
 
   const cachedMethodHandler = (
-    method: string,
-    params: unknown[],
-    target: any,
-    args: any[],
+    fn: ProviderInterface['send'],
+    args: Parameters<ProviderInterface['send']>,
   ) => {
+    const [method, params] = args;
     // If there are no parameters then we don't cache as we want the latest results
     if (!params.length) {
-      return Reflect.apply(target, provider, args);
+      return fn(...args);
     }
 
     const cacheKey = `${method}-${params[0]}`;
@@ -32,38 +30,22 @@ export function createCachedProvider(
       return cacheMap.get(cacheKey);
     }
 
-    const resultPromise: Promise<any> = Reflect.apply(target, provider, args);
+    const resultPromise = fn(...args);
     cacheMap.set(cacheKey, resultPromise);
     return resultPromise;
   };
 
-  return new Proxy(provider, {
-    get: function (target, prop, receiver) {
-      if (prop === 'send') {
-        return function (
-          method: string,
-          params: unknown[],
-          isCacheable: boolean,
-          subscription: boolean,
-        ) {
-          //caching state_getRuntimeVersion and chain_getHeader
-          //because they are fetched twice per block
-          if (['state_getRuntimeVersion', 'chain_getHeader'].includes(method)) {
-            return cachedMethodHandler(
-              method,
-              params,
-              target.send.bind(target),
-              arguments as unknown as any[],
-            );
-          }
+  const originalSend = provider.send.bind(provider);
+  (provider as any).send = (...args: Parameters<ProviderInterface['send']>) => {
+    const [method] = args;
+    //caching state_getRuntimeVersion and chain_getHeader
+    //because they are fetched twice per block
+    if (['state_getRuntimeVersion', 'chain_getHeader'].includes(method)) {
+      return cachedMethodHandler(originalSend, args);
+    }
 
-          // For other methods, just forward the call to the original method.
-          return Reflect.apply(target.send.bind(target), provider, arguments);
-        };
-      }
+    return originalSend(...args);
+  };
 
-      // For other properties, just return the original value.
-      return Reflect.get(target, prop, receiver);
-    },
-  });
+  return provider;
 }
