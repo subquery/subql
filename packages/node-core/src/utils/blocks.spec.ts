c9db561d8e8ba387751ebf271bcd3013636e0ea2
Scott Twiname <skott.twiname@gmail.com>
2024-09-27 16:49:49 +1200

    Rework bypass blocks to not expand large block ranges

--- packages/node-core/src/utils/blocks.spec.ts
+++ packages/node-core/src/utils/blocks.spec.ts
@@ -2,28 +2,28 @@
 // SPDX-License-Identifier: GPL-3.0
 
 import {range} from 'lodash';
-import {transformBypassBlocks, cleanedBatchBlocks} from './blocks';
+import {BypassBlocks} from '../indexer';
+import {filterBypassBlocks} from './blocks';
 
 describe('bypass logic', () => {
   it('process bypassBlocks with ranges', () => {
-    let bypassBlocks = transformBypassBlocks([20, 40, '5-10', 20, 140]);
-    expect(bypassBlocks).toEqual([20, 40, 5, 6, 7, 8, 9, 10, 140]);
+    let bypassBlocks: BypassBlocks = [20, 40, '5-10', 20, 140];
     let currentBlockBatch = [1, 5, 7, 8, 20, 40, 100, 120];
-    const case_1 = cleanedBatchBlocks(bypassBlocks, currentBlockBatch);
+    const case_1 = filterBypassBlocks(currentBlockBatch, bypassBlocks);
 
     expect(case_1).toEqual([1, 100, 120]);
 
-    bypassBlocks = transformBypassBlocks([' 5 - 10 ', 20, 140]);
+    bypassBlocks = [' 5 - 10 ', 20, 140];
     currentBlockBatch = [1, 5, 7, 8, 20, 40, 100, 120];
-    const case_2 = cleanedBatchBlocks(bypassBlocks, currentBlockBatch);
+    const case_2 = filterBypassBlocks(currentBlockBatch, bypassBlocks);
 
     expect(case_2).toEqual([1, 40, 100, 120]);
   });
 
   it('cleanedBatchBlocks with large amount blocks should not throw error Maximum call stack size exceeded', () => {
-    const bypassBlocks = transformBypassBlocks(['50051722-54939220']);
+    const bypassBlocks: BypassBlocks = ['50051722-54939220'];
     const currentBlockBatch = range(34312396, 34312495);
-    const case_1 = cleanedBatchBlocks(bypassBlocks, currentBlockBatch);
+    const case_1 = filterBypassBlocks(currentBlockBatch, bypassBlocks);
     expect(case_1).toEqual(currentBlockBatch);
   });
 });
