c42541af5f8cadf7bf563497b36e9a6c51399336
Tate <yongzhao.tan@onfinality.io>
2024-11-21 08:11:34 +0000

    unit test fix

--- packages/node/src/configure/SchemaMigration.service.test.ts
+++ packages/node/src/configure/SchemaMigration.service.test.ts
@@ -138,7 +138,7 @@ describe('SchemaMigration integration tests', () => {
 
     projectService = app.get('IProjectService');
     const projectUpgradeService = app.get('IProjectUpgradeService');
-    const storeCache = app.get(StoreCacheService);
+    const storeCache = app.get('IStoreModelProvider');
     const apiService = app.get(ApiService);
 
     await apiService.init();
