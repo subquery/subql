a7d14daf2803bb810b233656477bca26954bd7b2
Scott Twiname <skott.twiname@gmail.com>
2024-12-11 09:29:50 +1300

    Address feedback

--- packages/utils/src/graphql/graphql.spec.ts
+++ packages/utils/src/graphql/graphql.spec.ts
@@ -1,7 +1,6 @@
 // Copyright 2020-2024 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: GPL-3.0
 
-import {buildSchema} from 'graphql';
 import gql from 'graphql-tag';
 import {getAllEntitiesRelations} from './entities';
 import {buildSchemaFromDocumentNode} from './schema';
