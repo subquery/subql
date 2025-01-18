d67cfab4e0ec85cf776f9001a900d72e0dfb16ea
VolodymyrBg <aqdrgg19@gmail.com>
2025-01-18 13:13:03 +0200

    Update blockRange.ts

--- packages/node-core/src/admin/blockRange.ts
+++ packages/node-core/src/admin/blockRange.ts
@@ -2,7 +2,30 @@
 // SPDX-License-Identifier: GPL-3.0
 
 import {Type} from 'class-transformer';
-import {IsInt, IsPositive, Min, IsOptional} from 'class-validator';
+import {IsInt, IsPositive, Min, IsOptional, ValidateIf, registerDecorator, ValidationOptions, ValidationArguments} from 'class-validator';
+
+export function IsGreaterThan(property: string, validationOptions?: ValidationOptions) {
+  return function (object: any, propertyName: string) {
+    registerDecorator({
+      name: 'isGreaterThan',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [property],
+      options: validationOptions,
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+          const relatedValue = (args.object as any)[relatedPropertyName];
+          return value === undefined || value > relatedValue;
+        },
+        defaultMessage(args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+          return `${args.property} must be greater than ${relatedPropertyName}`;
+        },
+      },
+    });
+  };
+}
 
 export interface BlockRangeDtoInterface {
   startBlock: number;
@@ -26,5 +49,6 @@ export class BlockRangeDto implements BlockRangeDtoInterface {
   @IsPositive()
   @Min(0)
   @IsOptional()
+  @IsGreaterThan('startBlock')
   endBlock: number | undefined;
 }
