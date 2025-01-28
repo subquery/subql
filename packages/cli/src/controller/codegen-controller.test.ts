9175c27bf6c31a843fa66e214224b80ebe5c6ebb
Scott Twiname <skott.twiname@gmail.com>
2025-01-28 13:33:13 +1300

    Fix test

--- packages/cli/src/controller/codegen-controller.test.ts
+++ packages/cli/src/controller/codegen-controller.test.ts
@@ -8,30 +8,28 @@ import {codegen} from './codegen-controller';
 
 jest.setTimeout(30000);
 
+const projectPath = path.join(__dirname, '../../test/schemaTest');
+
 describe('Codegen can generate schema', () => {
   afterEach(async () => {
     await rimraf(path.join(__dirname, '../../test/schemaTest/src'));
   });
 
   it('codegen with correct schema should pass', async () => {
-    const projectPath = path.join(__dirname, '../../test/schemaTest');
     await expect(codegen(projectPath)).resolves.not.toThrow();
   });
 
   it('codegen with incorrect schema field should fail', async () => {
-    const projectPath = path.join(__dirname, '../../test/schemaTest');
     await expect(codegen(projectPath, ['project-bad-schema.yaml'])).rejects.toThrow(/is not an valid type/);
   });
 
   it('codegen with entities that uses reserved names should throw', async () => {
-    const projectPath = path.join(__dirname, '../../test/schemaTest');
     await expect(codegen(projectPath, ['project-bad-entity.yaml'])).rejects.toThrow(
       'EntityName: exampleEntityFilter cannot end with reservedKey: filter'
     );
   });
 
   it('Codegen should be able to generate ABIs from template datasources', async () => {
-    const projectPath = path.join(__dirname, '../../test/schemaTest');
     await codegen(projectPath, ['project-templates-abi.yaml']);
     await expect(
       fs.promises.readFile(`${projectPath}/src/types/abi-interfaces/Erc721.ts`, 'utf8')
@@ -39,12 +37,10 @@ describe('Codegen can generate schema', () => {
   });
 
   it('Should not fail, if ds does not have any assets', async () => {
-    const projectPath = path.join(__dirname, '../../test/schemaTest');
     await expect(codegen(projectPath, ['project-no-assets.yaml'])).resolves.not.toThrow();
   });
 
   it('Codegen should be able to generate ABIs from customName datasources', async () => {
-    const projectPath = path.join(__dirname, '../../test/schemaTest');
     await codegen(projectPath);
     await expect(
       fs.promises.readFile(`${projectPath}/src/types/abi-interfaces/Erc721.ts`, 'utf8')
@@ -52,16 +48,14 @@ describe('Codegen can generate schema', () => {
   });
 
   it('Should clean out existing types directory', async () => {
-    const projectPath = path.join(__dirname, '../../test/schemaTest');
     await codegen(projectPath);
     await codegen(projectPath, ['project-no-abi.yaml']);
 
     // should not contain abi directory
-    await expect(fs.promises.readFile(`${projectPath}/src/types/abi-interfaces/Erc721.ts`, 'utf8')).rejects.toThrow();
+    await expect(fs.promises.readFile(`${projectPath}/src/types/abi-interfaces/erc721.ts`, 'utf8')).rejects.toThrow();
   });
 
   it('should generate contracts on different glob paths', async () => {
-    const projectPath = path.join(__dirname, '../../test/schemaTest');
     await codegen(projectPath, ['typechain-test.yaml']);
 
     await expect(
@@ -78,19 +72,16 @@ describe('Codegen can generate schema', () => {
   });
 
   it('Should not generate ABI for non evm ds', async () => {
-    const projectPath = path.join(__dirname, '../../test/schemaTest');
     await codegen(projectPath, ['non-evm-project.yaml']);
     expect(fs.existsSync(`${projectPath}/src/types/abi-interfaces/`)).toBeFalsy();
   });
 
   it('Should not generate proto-interfaces if no chaintypes are provided', async () => {
-    const projectPath = path.join(__dirname, '../../test/schemaTest');
     await codegen(projectPath, ['project-cosmos.yaml']);
     expect(fs.existsSync(`${projectPath}/src/types/proto-interfaces/`)).toBeFalsy();
   });
 
   it('Should dedupe enums', async () => {
-    const projectPath = path.join(__dirname, '../../test/schemaTest');
     await codegen(projectPath, ['project-duplicate-enum.yaml']);
 
     const fooFile = await fs.promises.readFile(`${projectPath}/src/types/models/Foo.ts`, 'utf8');
@@ -104,7 +95,6 @@ describe('Codegen can generate schema', () => {
 
   // github issue #2211
   it('codegen file should import model files with correct case-sensitive names', async () => {
-    const projectPath = path.join(__dirname, '../../test/schemaTest');
     await codegen(projectPath, ['project-case-sensitive-import-entity.yaml']);
 
     const codegenFile = await fs.promises.readFile(`${projectPath}/src/types/models/index.ts`, 'utf8');
