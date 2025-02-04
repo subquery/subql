388758777736c8770cf69bf75ad8d4c992e65da4
Scott Twiname <skott.twiname@gmail.com>
2025-02-05 09:53:45 +1300

    Fix build issue

--- packages/node/src/indexer/runtime/runtime.service.spec.ts
+++ packages/node/src/indexer/runtime/runtime.service.spec.ts
@@ -118,7 +118,7 @@ describe('Runtime service', () => {
     (dictionaryService as any).useDictionary = (height: number) => true;
 
     // This is called in fetchService.preLoopHook
-    await runtimeService.syncDictionarySpecVersions(29233);
+    await runtimeService.syncDictionarySpecVersions();
 
     const metaSpy = jest.spyOn(apiService.api.rpc.state, 'getRuntimeVersion');
 
