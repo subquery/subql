ed59b7735d7f2f3d629f5134c97070271637e959
Scott Twiname <skott.twiname@gmail.com>
2025-04-03 10:03:01 +1300

    Fix typo

--- packages/node-core/src/indexer/blockDispatcher/base-block-dispatcher.ts
+++ packages/node-core/src/indexer/blockDispatcher/base-block-dispatcher.ts
@@ -317,7 +317,7 @@ export abstract class BaseBlockDispatcher<Q extends IQueue, DS, B> implements IB
           return;
         }
 
-        // Block fetching has failed, start shutting down things. But we can wait until the ramining fetched blocks can be processed
+        // Block fetching has failed, start shutting down things. But we can wait until the remaining fetched blocks can be processed
         logger.error(`Failed to fetch block, waiting for fetched blocks to be processed before shutting down.`, e);
         if (!this.isShutdown) {
           this.isShutdown = true;
