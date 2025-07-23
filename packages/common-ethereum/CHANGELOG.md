778d3d7f589751ad3f50368c8f96cbf10e0e46b7
Scott Twiname <skott.twiname@gmail.com>
2025-07-23 17:03:21 +1200

    Fix templates being excluded from @subql/common-ethereum package (#388)
    * Fix templates being excluded from @subql/common-ethereum package
    
    * Update docker building to reduce file size
    
    * Update changelogs

--- packages/common-ethereum/CHANGELOG.md
+++ packages/common-ethereum/CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Fixed
+- Templates being excluded from published packages (#388)
 
 ## [4.9.1] - 2025-07-01
 ### Changed
--- scripts/build.sh
+++ scripts/build.sh
@@ -3,16 +3,22 @@ set -e
 
 apk add --no-cache jq
 npm install -g --force yarn@latest
+
 cd "$1"
 
-# Modifies the package.json to replace "workspace:*" versions with actual versions
+# Replace "workspace:*" versions with actual versions
 jq -r '.dependencies | to_entries[] | select(.value == "workspace:*") | .key' package.json | while read -r dep; do
   directory=$(jq --arg dep "$dep" -r '.compilerOptions.paths[$dep][0]' ../../tsconfig.json | cut -d'/' -f 2)
   version=$(jq --arg directory "$directory" -r '.version' ../"$directory"/package.json)
   if [ "$version" != null ]; then
-    jq --arg dep "$dep" --arg version "$version" -r '.dependencies[$dep] = $version' package.json > package.tmp.json && mv package.tmp.json package.json
+    jq --arg dep "$dep" --arg version "$version" \
+      -r '.dependencies[$dep] = $version' package.json \
+      > package.tmp.json && mv package.tmp.json package.json
   fi
 done
 
+yarn install --production --frozen-lockfile
+
 yarn pack --filename app.tgz
-rm -rf /root/.npm /root/.cache
+
+rm -rf node_modules /root/.npm /root/.cache
