e5d2fe91b5164e3501e2d0568a6211aa49a2fb00
Scott Twiname <skott.twiname@gmail.com>
2025-09-03 08:38:57 +1200

    Work around etherscan api request limit

--- packages/cli/src/utils/etherscan.spec.ts
+++ packages/cli/src/utils/etherscan.spec.ts
@@ -2,22 +2,28 @@
 // SPDX-License-Identifier: GPL-3.0
 
 import {fetchContractDeployHeight, tryFetchAbiFromExplorer} from './etherscan';
+import {delay} from './utils';
+
+// Delays are added to avoid hitting rate limits on the free tier of Etherscan APIs
 
 describe('etherscan api', () => {
   describe('ABI fetching', () => {
     it('can fetch the ABI', async () => {
       const abi = await tryFetchAbiFromExplorer('0x53A270147832Fc4eb962584911F5D55e86b1BA3F', '8453' /* Base */);
       expect(abi).toMatchSnapshot();
+      await delay(0.5);
     });
 
     it('returns undefined if an ABI has not been uploaded', async () => {
       const abi = await tryFetchAbiFromExplorer('0x4eE879f39Cce3C4CA80E2ee90F9Df5aFEEaeb220', '1' /* Ethereum */);
       expect(abi).toBeUndefined();
+      await delay(0.5);
     });
 
     it('throws if a contract is non-existant', async () => {
       const abi = await tryFetchAbiFromExplorer('0x53A270147832Fc4eb962584911F5D55e86b1BA3F', '1' /* Ethereum */);
       expect(abi).toBeUndefined();
+      await delay(0.5);
     });
 
     it('throws for a network that is not supported', async () => {
@@ -26,6 +32,7 @@ describe('etherscan api', () => {
       ).rejects.toThrow(
         'Etherscan API error: Missing or unsupported chainid parameter (required for v2 api), please see https://api.etherscan.io/v2/chainlist for the list of supported chainids'
       );
+      await delay(0.5);
     });
   });
 
@@ -34,11 +41,13 @@ describe('etherscan api', () => {
       const height = await fetchContractDeployHeight('0x53A270147832Fc4eb962584911F5D55e86b1BA3F', '8453' /* Base */);
 
       expect(height).toBe(22771721);
+      await delay(0.5);
     });
 
     it('returns undefined when getting the deploy height for a non-existant contract', async () => {
       const height = await fetchContractDeployHeight('0x53A270147832Fc4eb962584911F5D55e86b1BA3F', '1' /* Ethereum */);
       expect(height).toBeUndefined();
+      await delay(0.5);
     });
   });
 });
