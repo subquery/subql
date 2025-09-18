3d4633114db7eb2b4d36eca91bd44957673b0f44
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-09-18 13:23:59 +0300

    fix: JSON and directory handling in scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,13 +6,17 @@ npm install -g --force yarn@latest
 cd "$1"
 
 # Modifies the package.json to replace "workspace:~" versions with actual versions
-jq -r '.dependencies | to_entries[] | select(.value == "workspace:~") | .key' package.json | while read -r dep; do
+while read -r dep; do
   directory=$(jq --arg dep "$dep" -r '.compilerOptions.paths[$dep][0]' ../../tsconfig.json | cut -d'/' -f 2)
-  version=$(jq --arg directory "$directory" -r '.version' ../"$directory"/package.json)
-  if [ "$version" != null ]; then
-    jq --arg dep "$dep" --arg version "$version" -r '.dependencies[$dep] = $version' package.json > package.tmp.json && mv package.tmp.json package.json
+  if [ -z "$directory" ]; then
+    continue
   fi
-done
+  version=$(jq -r --arg directory "$directory" '.version' "../$directory/package.json")
+  if [ -n "$version" ] && [ "$version" != "null" ]; then
+    jq --arg dep "$dep" --arg version "$version" '.dependencies[$dep] = $version' package.json > package.tmp.json
+    mv package.tmp.json package.json
+  fi
+done << <(jq -r '.dependencies | to_entries[] | select(.value == "workspace:~") | .key' package.json)
 
 yarn pack --filename app.tgz
 rm -rf /root/.npm /root/.cache
